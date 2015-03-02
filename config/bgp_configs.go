c6fc099c6a30e9cbea473fae5530d33bc6f6172b
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2015-03-02 14:24:48 +0900

    config: remove starttime in BgpGlobalStateType struct
    It's not used. We'll add again when it'll become necessary. This is
    part of work to remove time.Time in bgp_config.go since no nice way to
    represent time.Time with yang.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- config/bgp_configs.go
+++ config/bgp_configs.go
@@ -732,8 +732,6 @@ type PeerGroupType struct {
 
 //struct for container bgp-global-state
 type BgpGlobalStateType struct {
-	// start time
-	StartTime time.Time
 }
 
 //struct for container confederation
