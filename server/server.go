0baf2d76d4ee165a7909dccb4b57b17087462318
ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>
2015-03-23 15:08:28 +0000

    server: use net.SplitHostPort
    Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

--- server/server.go
+++ server/server.go
@@ -25,7 +25,6 @@ import (
 	"net"
 	"os"
 	"strconv"
-	"strings"
 )
 
 type serverMsgType int
@@ -145,13 +144,7 @@ func (server *BgpServer) Serve() {
 	for {
 		select {
 		case conn := <-acceptCh:
-			remoteAddr := func(addrPort string) string {
-				if strings.Index(addrPort, "[") == -1 {
-					return strings.Split(addrPort, ":")[0]
-				}
-				idx := strings.LastIndex(addrPort, ":")
-				return addrPort[1 : idx-1]
-			}(conn.RemoteAddr().String())
+			remoteAddr, _, _ := net.SplitHostPort(conn.RemoteAddr().String())
 			info, found := server.peerMap[remoteAddr]
 			if found {
 				log.Info("accepted a new passive connection from ", remoteAddr)
