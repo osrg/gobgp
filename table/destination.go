d8cc95a015bc3d6f5fb2ecf8771516b3b0914f36
ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>
2015-09-08 11:13:17 +0900

    table: support addpath recv feature
    distinguish paths which have differenct path-id

--- table/destination.go
+++ table/destination.go
@@ -264,8 +264,8 @@ func (dest *Destination) explicitWithdraw() paths {
 	for _, withdraw := range dest.withdrawList {
 		isFound := false
 		for _, path := range dest.knownPathList {
-			// We have a match if the source are same.
-			if path.GetSource().Equal(withdraw.GetSource()) {
+			// We have a match if the source and path-id are same.
+			if path.GetSource().Equal(withdraw.GetSource()) && path.GetNlri().PathIdentifier() == withdraw.GetNlri().PathIdentifier() {
 				isFound = true
 				path.IsWithdraw = true
 				matches = append(matches, path)
@@ -324,7 +324,7 @@ func (dest *Destination) implicitWithdraw() {
 			// version num. as newPaths are implicit withdrawal of old
 			// paths and when doing RouteRefresh (not EnhancedRouteRefresh)
 			// we get same paths again.
-			if newPath.GetSource().Equal(path.GetSource()) {
+			if newPath.GetSource().Equal(path.GetSource()) && newPath.GetNlri().PathIdentifier() == path.GetNlri().PathIdentifier() {
 				log.WithFields(log.Fields{
 					"Topic": "Table",
 					"Key":   dest.GetNlri().String(),
