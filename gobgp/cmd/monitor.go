c9262d33b3dccba3dba46063d0db206448f2feea
ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>
2015-09-22 23:41:39 +0900

    cli: show mpls label in vrf rib view
    Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

--- gobgp/cmd/monitor.go
+++ gobgp/cmd/monitor.go
@@ -63,7 +63,7 @@ func NewMonitorCmd() *cobra.Command {
 					j, _ := json.Marshal(p)
 					fmt.Println(string(j))
 				} else {
-					showRoute([]*Path{p}, false, false, true, true)
+					showRoute([]*Path{p}, false, false, false, true, true)
 				}
 			}
 
