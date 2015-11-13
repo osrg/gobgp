9d0b1373f0096b46ece90da2dc412c812b6bb8fe
ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>
2015-11-13 14:26:04 +0900

    server: support Neighbor.Trasnport configuration via grpc
    Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

--- server/server.go
+++ server/server.go
@@ -1839,6 +1839,10 @@ func (server *BgpServer) handleGrpcModNeighbor(grpcReq *GrpcRequest) (sMsgs []*S
 					}
 				}
 			}
+			if a.Transport != nil {
+				pconf.Transport.TransportConfig.LocalAddress = net.ParseIP(a.Transport.LocalAddress)
+				pconf.Transport.TransportConfig.PassiveMode = a.Transport.PassiveMode
+			}
 			return pconf
 		}
 		configneigh := apitoConfig(arg.Peer)
