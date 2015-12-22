34849c009789a60510eb53abbe312d08012f52a9
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2015-12-22 15:42:57 +0900

    table: fix the number of accepted
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- table/adj.go
+++ table/adj.go
@@ -49,7 +49,7 @@ func (adj *AdjRib) Update(pathList []*Path) {
 		if path.IsWithdraw {
 			if found {
 				delete(adj.table[rf], key)
-				if old.Filtered(adj.id) > POLICY_DIRECTION_NONE {
+				if old.Filtered(adj.id) == POLICY_DIRECTION_NONE {
 					adj.accepted[rf]--
 				}
 			}
