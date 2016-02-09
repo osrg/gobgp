86a24cbecc1b78c25baa95df25445f97977245cd
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2016-02-09 19:50:55 +0900

    table: avoid policy operations for eor path
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- table/path.go
+++ table/path.go
@@ -292,11 +292,16 @@ func (path *Path) SetUUID(uuid []byte) {
 }
 
 func (path *Path) Filter(id string, reason PolicyDirection) {
-	path.filtered[id] = reason
+	if path.eor == false {
+		path.filtered[id] = reason
+	}
 }
 
 func (path *Path) Filtered(id string) PolicyDirection {
-	return path.filtered[id]
+	if path.eor == false {
+		return path.filtered[id]
+	}
+	return POLICY_DIRECTION_NONE
 }
 
 func (path *Path) GetRouteFamily() bgp.RouteFamily {
