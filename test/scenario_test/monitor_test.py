osrg/gobgp

commit e6682c52ba3e09c4111bc94c938909cdcacd7d72

server: update path-attributes when peer is disconnected
close #807

also add a test to check this issue is fixed

Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

server/server.go
@@ -531,7 +531,6 @@ func filterpath(peer *Peer, path *table.Path) *table.Path {
 func (server *BgpServer) dropPeerAllRoutes(peer *Peer, families []bgp.RouteFamily) []*SenderMsg {
 	msgs := make([]*SenderMsg, 0)
 
-	options := &table.PolicyOptions{}
 	ids := make([]string, 0, len(server.neighborMap))
 	if peer.isRouteServerClient() {
 		for _, targetPeer := range server.neighborMap {
@@ -547,55 +546,12 @@ func (server *BgpServer) dropPeerAllRoutes(peer *Peer, families []bgp.RouteFamil
 		best, withdrawn := server.globalRib.DeletePathsByPeer(ids, peer.fsm.peerInfo, rf)
 		server.validatePaths(nil, withdrawn, true)
 
-		if peer.isRouteServerClient() {
-			for _, targetPeer := range server.neighborMap {
-				if !targetPeer.isRouteServerClient() || targetPeer == peer || targetPeer.fsm.state != bgp.BGP_FSM_ESTABLISHED {
-					continue
-				}
-				if _, ok := targetPeer.fsm.rfMap[rf]; !ok {
-					continue
-				}
-				pathList := make([]*table.Path, 0, len(best[targetPeer.TableID()]))
-				options.Neighbor = targetPeer.fsm.peerInfo.Address
-				for _, bestPath := range best[targetPeer.TableID()] {
-					if path := server.policy.ApplyPolicy(targetPeer.TableID(), table.POLICY_DIRECTION_EXPORT, filterpath(targetPeer, bestPath), options); path != nil {
-						pathList = append(pathList, path)
-					}
-				}
-				msgList := table.CreateUpdateMsgFromPaths(pathList)
-				msgs = append(msgs, newSenderMsg(targetPeer, msgList))
-				targetPeer.adjRibOut.Update(pathList)
+		for _, targetPeer := range server.neighborMap {
+			if peer.isRouteServerClient() != targetPeer.isRouteServerClient() || targetPeer == peer {
+				continue
 			}
-		} else {
-			sendPathList := make([]*table.Path, 0, len(best[table.GLOBAL_RIB_NAME]))
-			for _, path := range best[table.GLOBAL_RIB_NAME] {
-				if path != nil {
-					sendPathList = append(sendPathList, path)
-				}
-			}
-			if len(sendPathList) == 0 {
-				return msgs
-			}
-
-			server.broadcastBests(sendPathList)
-
-			for _, targetPeer := range server.neighborMap {
-				if targetPeer.isRouteServerClient() || targetPeer.fsm.state != bgp.BGP_FSM_ESTABLISHED {
-					continue
-				}
-				if _, ok := targetPeer.fsm.rfMap[rf]; !ok {
-					continue
-				}
-				pathList := make([]*table.Path, 0, len(sendPathList))
-				options.Neighbor = targetPeer.fsm.peerInfo.Address
-				for _, path := range sendPathList {
-					if path := server.policy.ApplyPolicy(table.GLOBAL_RIB_NAME, table.POLICY_DIRECTION_EXPORT, filterpath(targetPeer, path), options); path != nil {
-						pathList = append(pathList, path)
-					}
-				}
-				targetPeer.adjRibOut.Update(pathList)
-				msgList := table.CreateUpdateMsgFromPaths(pathList)
-
+			if paths := targetPeer.processOutgoingPaths(best[targetPeer.TableID()]); len(paths) > 0 {
+				msgList := table.CreateUpdateMsgFromPaths(paths)
 				msgs = append(msgs, newSenderMsg(targetPeer, msgList))
 			}
 		}
@@ -632,6 +588,9 @@ func (server *BgpServer) broadcastValidationResults(results []*api.ROAResult) {
 
 func (server *BgpServer) broadcastBests(bests []*table.Path) {
 	for _, path := range bests {
+		if path == nil {
+			continue
+		}
 		if !path.IsFromExternal() {
 			z := newBroadcastZapiBestMsg(server.zclient, path)
 			if z != nil {
@@ -813,10 +772,10 @@ func (server *BgpServer) validatePaths(newly, withdrawn []*table.Path, peerDown 
 }
 
 func (server *BgpServer) propagateUpdate(peer *Peer, pathList []*table.Path) ([]*SenderMsg, []*table.Path) {
-	msgs := make([]*SenderMsg, 0)
 	rib := server.globalRib
-	var alteredPathList []*table.Path
-	options := &table.PolicyOptions{}
+	var alteredPathList, newly, withdrawn []*table.Path
+	var best map[string][]*table.Path
+
 	if peer != nil && peer.isRouteServerClient() {
 		for _, path := range pathList {
 			path.Filter(peer.ID(), table.POLICY_DIRECTION_IMPORT)
@@ -830,10 +789,7 @@ func (server *BgpServer) propagateUpdate(peer *Peer, pathList []*table.Path) ([]
 			moded = append(moded, server.RSimportPaths(targetPeer, pathList)...)
 		}
 		isTarget := func(p *Peer) bool {
-			if !p.isRouteServerClient() || p.fsm.state != bgp.BGP_FSM_ESTABLISHED || p.fsm.pConf.GracefulRestart.State.LocalRestarting {
-				return false
-			}
-			return true
+			return p.isRouteServerClient() && p.fsm.state == bgp.BGP_FSM_ESTABLISHED && !p.fsm.pConf.GracefulRestart.State.LocalRestarting
 		}
 
 		ids := make([]string, 0, len(server.neighborMap))
@@ -842,71 +798,30 @@ func (server *BgpServer) propagateUpdate(peer *Peer, pathList []*table.Path) ([]
 				ids = append(ids, targetPeer.TableID())
 			}
 		}
-		best, newly, withdrawn := rib.ProcessPaths(ids, append(pathList, moded...))
+		best, newly, withdrawn = rib.ProcessPaths(ids, append(pathList, moded...))
 		server.validatePaths(newly, withdrawn, false)
-
-		for _, targetPeer := range server.neighborMap {
-			if !isTarget(targetPeer) {
-				continue
-			}
-			sendPathList := make([]*table.Path, 0, len(best[targetPeer.TableID()]))
-			options.Neighbor = targetPeer.fsm.peerInfo.Address
-			for _, bestPath := range best[targetPeer.TableID()] {
-				path := server.policy.ApplyPolicy(targetPeer.TableID(), table.POLICY_DIRECTION_EXPORT, filterpath(targetPeer, bestPath), options)
-				if path != nil {
-					sendPathList = append(sendPathList, path)
-				}
-			}
-			msgList := table.CreateUpdateMsgFromPaths(sendPathList)
-			targetPeer.adjRibOut.Update(sendPathList)
-			msgs = append(msgs, newSenderMsg(targetPeer, msgList))
-		}
 	} else {
 		for idx, path := range pathList {
 			pathList[idx] = server.policy.ApplyPolicy(table.GLOBAL_RIB_NAME, table.POLICY_DIRECTION_IMPORT, path, nil)
 		}
 		alteredPathList = pathList
-		best, newly, withdrawn := rib.ProcessPaths([]string{table.GLOBAL_RIB_NAME}, pathList)
+		best, newly, withdrawn = rib.ProcessPaths([]string{table.GLOBAL_RIB_NAME}, pathList)
 		server.validatePaths(newly, withdrawn, false)
-		sendPathList := make([]*table.Path, 0, len(best[table.GLOBAL_RIB_NAME]))
-		if server.bgpConfig.Global.Collector.Enabled {
-			sendPathList = pathList
-		} else {
-			for _, path := range best[table.GLOBAL_RIB_NAME] {
-				if path != nil {
-					sendPathList = append(sendPathList, path)
-				}
-			}
-			if len(sendPathList) == 0 {
-				return msgs, alteredPathList
-			}
-			server.broadcastBests(sendPathList)
+		if len(best[table.GLOBAL_RIB_NAME]) == 0 {
+			return nil, alteredPathList
 		}
+		if !server.bgpConfig.Global.Collector.Enabled {
+			server.broadcastBests(best[table.GLOBAL_RIB_NAME])
+		}
+	}
 
-		for _, targetPeer := range server.neighborMap {
-			if targetPeer.isRouteServerClient() || targetPeer.fsm.state != bgp.BGP_FSM_ESTABLISHED || targetPeer.fsm.pConf.GracefulRestart.State.LocalRestarting {
-				if targetPeer.fsm.pConf.GracefulRestart.State.LocalRestarting {
-					log.WithFields(log.Fields{
-						"Topic": "Peer",
-						"Key":   targetPeer.conf.Config.NeighborAddress,
-					}).Debug("now syncing, suppress sending updates")
-				}
-				continue
-			}
-			pathList := make([]*table.Path, len(sendPathList))
-			copy(pathList, sendPathList)
-			options.Neighbor = targetPeer.fsm.peerInfo.Address
-			for idx, path := range pathList {
-				path = server.policy.ApplyPolicy(table.GLOBAL_RIB_NAME, table.POLICY_DIRECTION_EXPORT, filterpath(targetPeer, path), options)
-				if path != nil && !server.bgpConfig.Global.Collector.Enabled {
-					path = path.Clone(path.IsWithdraw)
-					path.UpdatePathAttrs(&server.bgpConfig.Global, &targetPeer.conf)
-				}
-				pathList[idx] = path
-			}
-			targetPeer.adjRibOut.Update(pathList)
-			msgList := table.CreateUpdateMsgFromPaths(pathList)
-
+	msgs := make([]*SenderMsg, 0)
+	for _, targetPeer := range server.neighborMap {
+		if (peer == nil && targetPeer.isRouteServerClient()) || (peer != nil && peer.isRouteServerClient() != targetPeer.isRouteServerClient()) {
+			continue
+		}
+		if paths := targetPeer.processOutgoingPaths(best[targetPeer.TableID()]); len(paths) > 0 {
+			msgList := table.CreateUpdateMsgFromPaths(paths)
 			msgs = append(msgs, newSenderMsg(targetPeer, msgList))
 		}
 	}

server/peer.go
@@ -165,6 +165,38 @@ func (peer *Peer) getBestFromLocal(rfList []bgp.RouteFamily) ([]*table.Path, []*
 	return pathList, filtered
 }
 
+func (peer *Peer) processOutgoingPaths(paths []*table.Path) []*table.Path {
+	if peer.fsm.state != bgp.BGP_FSM_ESTABLISHED {
+		return nil
+	}
+	if peer.fsm.pConf.GracefulRestart.State.LocalRestarting {
+		log.WithFields(log.Fields{
+			"Topic": "Peer",
+			"Key":   peer.conf.Config.NeighborAddress,
+		}).Debug("now syncing, suppress sending updates")
+		return nil
+	}
+
+	outgoing := make([]*table.Path, 0, len(paths))
+	options := &table.PolicyOptions{
+		Neighbor: peer.fsm.peerInfo.Address,
+	}
+	for _, path := range paths {
+		path = peer.policy.ApplyPolicy(peer.TableID(), table.POLICY_DIRECTION_EXPORT, filterpath(peer, path), options)
+		if path == nil {
+			continue
+		}
+		if !peer.isRouteServerClient() && !peer.gConf.Collector.Enabled {
+			path = path.Clone(path.IsWithdraw)
+			path.UpdatePathAttrs(&peer.gConf, &peer.conf)
+		}
+		outgoing = append(outgoing, path)
+	}
+
+	peer.adjRibOut.Update(outgoing)
+	return outgoing
+}
+
 func (peer *Peer) handleBGPmessage(e *FsmMsg) ([]*table.Path, []*bgp.BGPMessage, []bgp.RouteFamily) {
 	m := e.MsgData.(*bgp.BGPMessage)
 	log.WithFields(log.Fields{

test/scenario_test/bgp_router_test.py
@@ -251,7 +251,7 @@ class GoBGPTestBase(unittest.TestCase):
 
     def test_15_check_active_connection(self):
         g1 = self.gobgp
-        g2 = GoBGPContainer(name='g2', asn=65000, router_id='192.168.0.5',
+        g2 = GoBGPContainer(name='g2', asn=65000, router_id='192.168.0.7',
                             ctn_image_name=self.gobgp.image,
                             log_level=parser_option.gobgp_log_level)
         time.sleep(g2.run())
@@ -284,6 +284,32 @@ class GoBGPTestBase(unittest.TestCase):
         self.assertTrue(local_pref['value'] == 100)
         med = extract_path_attribute(path, BGP_ATTR_TYPE_MULTI_EXIT_DISC)
         self.assertTrue(med['metric'] == 2000)
+
+    def test_17_check_shutdown(self):
+        q1 = self.quaggas['q1']
+        q2 = self.quaggas['q2']
+        q3 = self.quaggas['q3']
+
+        q2.add_route('20.0.0.0/24')
+        q3.add_route('20.0.0.0/24')
+
+        self.test_01_neighbor_established()
+
+        self.test_02_check_gobgp_global_rib()
+
+        paths = q1.get_global_rib('20.0.0.0/24')
+        self.assertTrue(len(paths) == 1)
+        n_addrs = [i[1].split('/')[0] for i in self.gobgp.ip_addrs]
+        self.assertTrue(paths[0]['nexthop'] in n_addrs)
+
+        q3.stop()
+
+        time.sleep(3)
+
+        paths = q1.get_global_rib('20.0.0.0/24')
+        self.assertTrue(len(paths) == 1)
+        self.assertTrue(paths[0]['nexthop'] in n_addrs)
+
 
 if __name__ == '__main__':
     if os.geteuid() is not 0:

commit e0077410c5ef7e3256df42667ca340985da0232f

server: add missing broadcastBests when dropping peer's routes
fix regression introduced by e6682c52ba3e09c4111bc94c938909cdcacd7d72

also add a test to check the behavior.

Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

server/server.go
@@ -546,6 +546,10 @@ func (server *BgpServer) dropPeerAllRoutes(peer *Peer, families []bgp.RouteFamil
 		best, withdrawn := server.globalRib.DeletePathsByPeer(ids, peer.fsm.peerInfo, rf)
 		server.validatePaths(nil, withdrawn, true)
 
+		if !peer.isRouteServerClient() && !server.bgpConfig.Global.Collector.Enabled {
+			server.broadcastBests(best[table.GLOBAL_RIB_NAME])
+		}
+
 		for _, targetPeer := range server.neighborMap {
 			if peer.isRouteServerClient() != targetPeer.isRouteServerClient() || targetPeer == peer {
 				continue

test/lib/base.py
@@ -17,6 +17,7 @@ from fabric.api import local, lcd
 from fabric import colors
 from fabric.utils import indent
 from fabric.state import env, output
+from docker import Client
 
 import netaddr
 import os
@@ -208,10 +209,14 @@ class Container(object):
         self.ip_addrs.append((intf_name, ip_addr, bridge.name))
         try_several_times(lambda :local(str(c)))
 
-    def local(self, cmd, capture=False, flag=''):
-        return local("docker exec {0} {1} {2}".format(flag,
-                                                      self.docker_name(),
-                                                      cmd), capture)
+    def local(self, cmd, capture=False, stream=False, detach=False):
+        if stream:
+            dckr = Client(timeout=120, version='auto')
+            i = dckr.exec_create(container=self.docker_name(), cmd=cmd)
+            return dckr.exec_start(i['Id'], tty=True, stream=stream, detach=detach)
+        else:
+            flag = '-d' if detach else ''
+            return local('docker exec {0} {1} {2}'.format(flag, self.docker_name(), cmd), capture)
 
     def get_pid(self):
         if self.is_running:

test/lib/exabgp.py
@@ -32,7 +32,7 @@ class ExaBGPContainer(BGPContainer):
         cmd << 'env exabgp.log.destination={0}/exabgpd.log'.format(self.SHARED_VOLUME)
         cmd << "exabgp.tcp.bind='0.0.0.0' exabgp.tcp.port=179"
         cmd << './exabgp/sbin/exabgp {0}/exabgpd.conf'.format(self.SHARED_VOLUME)
-        self.local(str(cmd), flag='-d')
+        self.local(str(cmd), detach=True)
 
     def _update_exabgp(self):
         if self.exabgp_path == '':

test/lib/gobgp.py
@@ -17,6 +17,8 @@ from base import *
 import json
 import toml
 from itertools import chain
+from threading import Thread
+import socket
 
 def extract_path_attribute(path, typ):
     for a in path['attrs']:
@@ -52,7 +54,7 @@ class GoBGPContainer(BGPContainer):
         local(cmd, capture=True)
         cmd = "chmod 755 {0}/start.sh".format(self.config_dir)
         local(cmd, capture=True)
-        self.local("{0}/start.sh".format(self.SHARED_VOLUME), flag='-d')
+        self.local("{0}/start.sh".format(self.SHARED_VOLUME), detach=True)
 
     def graceful_restart(self):
         self.local("pkill -INT gobgpd")
@@ -61,7 +63,7 @@ class GoBGPContainer(BGPContainer):
         cmd = 'cp {0}/zebra.conf {1}/'.format(self.SHARED_VOLUME, self.QUAGGA_VOLUME)
         self.local(cmd)
         cmd = '/usr/lib/quagga/zebra -f {0}/zebra.conf'.format(self.QUAGGA_VOLUME)
-        self.local(cmd, flag='-d')
+        self.local(cmd, detach=True)
 
     def run(self):
         super(GoBGPContainer, self).run()
@@ -124,6 +126,29 @@ class GoBGPContainer(BGPContainer):
                 p["nexthop"] = self._get_nexthop(p)
                 p["aspath"] = self._get_as_path(p)
         return ret
+
+    def monitor_global_rib(self, queue, rf='ipv4'):
+        def monitor():
+            it = self.local('gobgp -j monitor global rib -a {0}'.format(rf), stream=True)
+            buf = ''
+            try:
+                for line in it:
+                    if line == '\n':
+                        p = json.loads(buf)[0]
+                        p["nexthop"] = self._get_nexthop(p)
+                        p["aspath"] = self._get_as_path(p)
+                        queue.put(p)
+                        buf = ''
+                    else:
+                        buf += line
+            except socket.timeout:
+                #self.local('pkill -x gobgp')
+                queue.put('timeout')
+                return
+
+        t = Thread(target=monitor)
+        t.daemon = True
+        t.start()
 
     def _get_adj_rib(self, adj_type, peer, prefix='', rf='ipv4'):
         if peer not in self.peers:

test/scenario_test/ci-scripts/travis-build-script.sh
@@ -21,7 +21,7 @@ sudo apt-get -q -y install iputils-arping bridge-utils lv
 sudo wget https://raw.github.com/jpetazzo/pipework/master/pipework -O /usr/local/bin/pipework
 sudo chmod 755 /usr/local/bin/pipework
 
-sudo -H pip --quiet install nose toml ciscoconfparse ecdsa "pycrypto>=2.1" fabric netaddr nsenter
+sudo -H pip --quiet install -r $GOBGP/test/pip-requires.txt
 
 ls -al
 git log | head -20
@@ -64,6 +64,9 @@ sudo  PYTHONPATH=$GOBGP/test python graceful_restart_test.py --gobgp-image $GOBG
 PIDS=("${PIDS[@]}" $!)
 
 sudo  PYTHONPATH=$GOBGP/test python bgp_zebra_test.py --gobgp-image $GOBGP_IMAGE --test-prefix zebra -x &
+PIDS=("${PIDS[@]}" $!)
+
+sudo  PYTHONPATH=$GOBGP/test python monitor_test.py --gobgp-image $GOBGP_IMAGE --test-prefix mon -x &
 PIDS=("${PIDS[@]}" $!)
 
 for (( i = 0; i < ${#PIDS[@]}; ++i ))

test/scenario_test/monitor_test.py
@@ -1,0 +1,112 @@
+# Copyright (C) 2016 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from fabric.api import local
+from lib import base
+from lib.gobgp import *
+from lib.quagga import *
+import sys
+import os
+import time
+import nose
+from noseplugin import OptionParser, parser_option
+from itertools import chain
+import Queue
+
+
+class GoBGPTestBase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        gobgp_ctn_image_name = parser_option.gobgp_image
+        base.TEST_PREFIX = parser_option.test_prefix
+
+        g1 = GoBGPContainer(name='g1', asn=65000, router_id='192.168.0.1',
+                            ctn_image_name=gobgp_ctn_image_name,
+                            log_level=parser_option.gobgp_log_level)
+        q1 = QuaggaBGPContainer(name='q1', asn=65001, router_id='192.168.0.2')
+        q2 = QuaggaBGPContainer(name='q2', asn=65002, router_id='192.168.0.3')
+        q3 = QuaggaBGPContainer(name='q3', asn=65003, router_id='192.168.0.4')
+
+        qs = [q1, q2, q3]
+        ctns = [g1, q1, q2, q3]
+
+        # advertise a route from q1, q2, q3
+        for idx, q in enumerate(qs):
+            route = '10.0.{0}.0/24'.format(idx+1)
+            q.add_route(route)
+
+        initial_wait_time = max(ctn.run() for ctn in ctns)
+
+        time.sleep(initial_wait_time)
+
+        for q in qs:
+            g1.add_peer(q, reload_config=False, passwd='passwd')
+            q.add_peer(g1, passwd='passwd', passive=True)
+
+        g1.create_config()
+        g1.reload_config()
+
+        cls.gobgp = g1
+        cls.quaggas = {'q1': q1, 'q2': q2, 'q3': q3}
+
+    def test_01_monitor_initial_adv(self):
+        qu = Queue.Queue()
+        self.gobgp.monitor_global_rib(qu)
+        for q in self.quaggas.itervalues():
+            self.gobgp.wait_for(expected_state=BGP_FSM_ESTABLISHED, peer=q)
+            print '{0} get established'.format(q.name)
+
+        cnt = 0
+
+        while True:
+            try:
+                info = qu.get(timeout=1)
+                if info == 'timeout':
+                    raise Exception('timeout')
+                cnt += 1
+                if cnt == len(self.quaggas):
+                    break
+            except Queue.Empty:
+                pass
+
+    def test_02_stop_q1(self):
+        qu = Queue.Queue()
+        self.gobgp.monitor_global_rib(qu)
+        self.quaggas['q1'].stop()
+
+        while True:
+            try:
+                info = qu.get(timeout=1)
+                if info == 'timeout':
+                    raise Exception('timeout')
+                self.assertTrue(info['isWithdraw'])
+                break
+            except Queue.Empty:
+                pass
+
+
+if __name__ == '__main__':
+    if os.geteuid() is not 0:
+        print "you are not root."
+        sys.exit(1)
+    output = local("which docker 2>&1 > /dev/null ; echo $?", capture=True)
+    if int(output) is not 0:
+        print "docker not found"
+        sys.exit(1)
+
+    nose.main(argv=sys.argv, addplugins=[OptionParser()],
+              defaultTest=sys.argv[0])