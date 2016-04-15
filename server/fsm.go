2767e4ac4feb8181e0634a11c8943dcac0da32c3
ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>
2016-04-15 08:43:48 +0000

    fsm: improve log message when prefix-limit reached
    Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

--- server/fsm.go
+++ server/fsm.go
@@ -1277,13 +1277,18 @@ func (h *FSMHandler) changeAdminState(s AdminState) error {
 				"Key":   fsm.pConf.Config.NeighborAddress,
 				"State": fsm.state,
 			}).Info("Administrative start")
-
-		case ADMIN_STATE_DOWN, ADMIN_STATE_PFX_CT:
+		case ADMIN_STATE_DOWN:
 			log.WithFields(log.Fields{
 				"Topic": "Peer",
 				"Key":   fsm.pConf.Config.NeighborAddress,
 				"State": fsm.state,
 			}).Info("Administrative shutdown")
+		case ADMIN_STATE_PFX_CT:
+			log.WithFields(log.Fields{
+				"Topic": "Peer",
+				"Key":   fsm.pConf.Config.NeighborAddress,
+				"State": fsm.state,
+			}).Info("Administrative shutdown(Prefix limit reached)")
 		}
 
 	} else {
