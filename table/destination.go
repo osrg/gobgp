c6ddfb1d8ecc5944a775a458fbce648713ae3b02
ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>
2016-04-20 08:03:54 +0000

    table: use received time as tie-breaker
    older path get preference
    
    can be disabled by
    `global.route-selection-options.external-compare-router-id = true`
    
    close #806
    
    Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

--- table/destination.go
+++ table/destination.go
@@ -27,6 +27,8 @@ import (
 	"sort"
 )
 
+var SelectionOptions config.RouteSelectionOptionsConfig
+
 type BestPathReason string
 
 const (
@@ -42,6 +44,7 @@ const (
 	BPR_ASN                BestPathReason = "ASN"
 	BPR_IGP_COST           BestPathReason = "IGP Cost"
 	BPR_ROUTER_ID          BestPathReason = "Router ID"
+	BPR_OLDER              BestPathReason = "Older"
 )
 
 func IpToRadixkey(b []byte, max uint8) string {
@@ -485,6 +488,10 @@ func (p paths) Less(i, j int) bool {
 		reason = BPR_IGP_COST
 	}
 	if better == nil {
+		better = compareByAge(path1, path2)
+		reason = BPR_OLDER
+	}
+	if better == nil {
 		var e error = nil
 		better, e = compareByRouterID(path1, path2)
 		if e != nil {
@@ -720,11 +727,11 @@ func compareByRouterID(path1, path2 *Path) (*Path, error) {
 
 	// If both paths are from eBGP peers, then according to RFC we need
 	// not tie break using router id.
-	if !path1.IsIBGP() && !path2.IsIBGP() {
+	if !SelectionOptions.ExternalCompareRouterId && !path1.IsIBGP() && !path2.IsIBGP() {
 		return nil, nil
 	}
 
-	if path1.IsIBGP() != path2.IsIBGP() {
+	if !SelectionOptions.ExternalCompareRouterId && path1.IsIBGP() != path2.IsIBGP() {
 		return nil, fmt.Errorf("This method does not support comparing ebgp with ibgp path")
 	}
 
@@ -741,6 +748,20 @@ func compareByRouterID(path1, path2 *Path) (*Path, error) {
 	} else {
 		return path2, nil
 	}
+}
+
+func compareByAge(path1, path2 *Path) *Path {
+	if !path1.IsIBGP() && !path2.IsIBGP() && !SelectionOptions.ExternalCompareRouterId {
+		age1 := path1.info.timestamp.UnixNano()
+		age2 := path2.info.timestamp.UnixNano()
+		if age1 == age2 {
+			return nil
+		} else if age1 < age2 {
+			return path1
+		}
+		return path2
+	}
+	return nil
 }
 
 func (dest *Destination) String() string {
