osrg/gobgp

commit 332766189685028c4f9852e4285fb1a9025223cc

server: send local withdrawal paths regardless of path calculation result
Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

server/peer.go
@@ -166,7 +166,7 @@ func (peer *Peer) getBestFromLocal(rfList []bgp.RouteFamily) ([]*table.Path, []*
 	return pathList, filtered
 }
 
-func (peer *Peer) processOutgoingPaths(paths []*table.Path) []*table.Path {
+func (peer *Peer) processOutgoingPaths(paths, withdrawals []*table.Path) []*table.Path {
 	if peer.fsm.state != bgp.BGP_FSM_ESTABLISHED {
 		return nil
 	}
@@ -179,6 +179,12 @@ func (peer *Peer) processOutgoingPaths(paths []*table.Path) []*table.Path {
 	}
 
 	outgoing := make([]*table.Path, 0, len(paths))
+	for _, path := range withdrawals {
+		if path.IsLocal() {
+			outgoing = append(outgoing, path)
+		}
+	}
+
 	options := &table.PolicyOptions{
 		Neighbor: peer.fsm.peerInfo.Address,
 	}

server/server.go
@@ -477,7 +477,7 @@ func (server *BgpServer) dropPeerAllRoutes(peer *Peer, families []bgp.RouteFamil
 			if peer.isRouteServerClient() != targetPeer.isRouteServerClient() || targetPeer == peer {
 				continue
 			}
-			if paths := targetPeer.processOutgoingPaths(best[targetPeer.TableID()]); len(paths) > 0 {
+			if paths := targetPeer.processOutgoingPaths(best[targetPeer.TableID()], nil); len(paths) > 0 {
 				msgs = append(msgs, newSenderMsg(targetPeer, paths, nil, false))
 			}
 		}
@@ -746,7 +746,7 @@ func (server *BgpServer) propagateUpdate(peer *Peer, pathList []*table.Path) ([]
 		if (peer == nil && targetPeer.isRouteServerClient()) || (peer != nil && peer.isRouteServerClient() != targetPeer.isRouteServerClient()) {
 			continue
 		}
-		if paths := targetPeer.processOutgoingPaths(best[targetPeer.TableID()]); len(paths) > 0 {
+		if paths := targetPeer.processOutgoingPaths(best[targetPeer.TableID()], withdrawn); len(paths) > 0 {
 			msgs = append(msgs, newSenderMsg(targetPeer, paths, nil, false))
 		}
 	}

test/scenario_test/bgp_router_test.py
@@ -303,12 +303,40 @@ class GoBGPTestBase(unittest.TestCase):
         self.assertTrue(paths[0]['nexthop'] in n_addrs)
 
         q3.stop()
+        del self.quaggas['q3']
 
         time.sleep(3)
 
         paths = q1.get_global_rib('20.0.0.0/24')
         self.assertTrue(len(paths) == 1)
         self.assertTrue(paths[0]['nexthop'] in n_addrs)
+
+    def test_18_check_withdrawal(self):
+        g1 = self.gobgp
+        q1 = self.quaggas['q1']
+        q2 = self.quaggas['q2']
+
+        g1.add_route('30.0.0.0/24')
+        q1.add_route('30.0.0.0/24')
+
+        self.test_01_neighbor_established()
+
+        self.test_02_check_gobgp_global_rib()
+
+        paths = g1.get_adj_rib_out(q1, '30.0.0.0/24')
+        self.assertTrue(len(paths) == 1)
+        self.assertTrue(paths[0]['source-id'] == '<nil>')
+        paths = g1.get_adj_rib_out(q2, '30.0.0.0/24')
+        self.assertTrue(len(paths) == 1)
+        self.assertTrue(paths[0]['source-id'] == '<nil>')
+
+        g1.local('gobgp global rib del 30.0.0.0/24')
+
+        paths = g1.get_adj_rib_out(q1, '30.0.0.0/24')
+        self.assertTrue(len(paths) == 0)
+        paths = g1.get_adj_rib_out(q2, '30.0.0.0/24')
+        self.assertTrue(len(paths) == 1)
+        self.assertTrue(paths[0]['source-id'] == '192.168.0.2')
 
 
 if __name__ == '__main__':

commit a52e54837dcbca49cd48acf9518fc051cbf048a5

server: fix advertising multiple local withdrawals with same prefix
a bug introduced by 332766189685028c4f9852e4285fb1a9025223cc

Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

server/peer.go
@@ -128,7 +128,7 @@ func (peer *Peer) getAccepted(rfList []bgp.RouteFamily) []*table.Path {
 	return peer.adjRibIn.PathList(rfList, true)
 }
 
-func (peer *Peer) filterpath(path *table.Path) *table.Path {
+func (peer *Peer) filterpath(path *table.Path, withdrawals []*table.Path) *table.Path {
 	// special handling for RTC nlri
 	// see comments in (*Destination).Calculate()
 	if path != nil && path.GetRouteFamily() == bgp.RF_RTC_UC && !path.IsWithdraw {
@@ -147,7 +147,7 @@ func (peer *Peer) filterpath(path *table.Path) *table.Path {
 			}
 		}
 	}
-	if filterpath(peer, path) == nil {
+	if path = filterpath(peer, path, withdrawals); path == nil {
 		return nil
 	}
 
@@ -172,7 +172,7 @@ func (peer *Peer) getBestFromLocal(rfList []bgp.RouteFamily) ([]*table.Path, []*
 	pathList := []*table.Path{}
 	filtered := []*table.Path{}
 	for _, path := range peer.localRib.GetBestPathList(peer.TableID(), rfList) {
-		if p := peer.filterpath(path); p != nil {
+		if p := peer.filterpath(path, nil); p != nil {
 			pathList = append(pathList, p)
 		} else {
 			filtered = append(filtered, path)
@@ -200,23 +200,9 @@ func (peer *Peer) processOutgoingPaths(paths, withdrawals []*table.Path) []*tabl
 	}
 
 	outgoing := make([]*table.Path, 0, len(paths))
-	// Note: multiple paths having the same prefix could exist the
-	// withdrawals list in the case of Route Server setup with
-	// import policies modifying paths. In such case, gobgp sends
-	// duplicated update messages; withdraw messages for the same
-	// prefix.
-	// However, currently we don't support local path for Route
-	// Server setup so this is NOT the case.
-	for _, path := range withdrawals {
-		if path.IsLocal() {
-			if _, ok := peer.fsm.rfMap[path.GetRouteFamily()]; ok {
-				outgoing = append(outgoing, path)
-			}
-		}
-	}
 
 	for _, path := range paths {
-		if p := peer.filterpath(path); p != nil {
+		if p := peer.filterpath(path, withdrawals); p != nil {
 			outgoing = append(outgoing, p)
 		}
 	}

server/server.go
@@ -270,7 +270,7 @@ func isASLoop(peer *Peer, path *table.Path) bool {
 	return false
 }
 
-func filterpath(peer *Peer, path *table.Path) *table.Path {
+func filterpath(peer *Peer, path *table.Path, withdrawals []*table.Path) *table.Path {
 	if path == nil {
 		return nil
 	}
@@ -352,6 +352,25 @@ func filterpath(peer *Peer, path *table.Path) *table.Path {
 	}
 
 	if peer.ID() == path.GetSource().Address.String() {
+		// Say, gobgp was advertising prefix A and peer P also.
+		// When gobgp withdraws prefix A, best path calculation chooses
+		// the path from P as the best path for prefix A.
+		// For peers other than P, this path should be advertised
+		// (as implicit withdrawal). However for P, we should advertise
+		// the local withdraw path.
+
+		// Note: multiple paths having the same prefix could exist the
+		// withdrawals list in the case of Route Server setup with
+		// import policies modifying paths. In such case, gobgp sends
+		// duplicated update messages; withdraw messages for the same
+		// prefix.
+		// However, currently we don't support local path for Route
+		// Server setup so this is NOT the case.
+		for _, w := range withdrawals {
+			if w.IsLocal() && path.GetNlri().String() == w.GetNlri().String() {
+				return w
+			}
+		}
 		log.WithFields(log.Fields{
 			"Topic": "Peer",
 			"Key":   peer.ID(),

test/lib/base.py
@@ -158,6 +158,7 @@ class Container(object):
         self.ip6_addrs = []
         self.is_running = False
         self.eths = []
+        self.tcpdump_running = False
 
         if self.docker_name() in get_containers():
             self.remove()
@@ -230,12 +231,21 @@ class Container(object):
             return int(local(cmd, capture=True))
         return -1
 
-    def start_tcpdump(self, interface=None, filename=None):
+    def start_tcpdump(self, interface=None, filename=None, expr='tcp port 179'):
+        if self.tcpdump_running:
+            raise Exception('tcpdump already running')
+        self.tcpdump_running = True
         if not interface:
             interface = "eth0"
         if not filename:
-            filename = "{0}/{1}.dump".format(self.shared_volumes[0][1], interface)
-        self.local("tcpdump -i {0} -w {1}".format(interface, filename), detach=True)
+            filename = '{0}.dump'.format(interface)
+        self.local("tcpdump -i {0} -w {1}/{2} {3}".format(interface, self.shared_volumes[0][1], filename, expr), detach=True)
+        return '{0}/{1}'.format(self.shared_volumes[0][0], filename)
+
+    def stop_tcpdump(self):
+        self.local("pkill tcpdump")
+        self.tcpdump_running = False
+
 
 class BGPContainer(Container):
 

test/scenario_test/bgp_router_test.py
@@ -25,6 +25,9 @@ import time
 import nose
 from noseplugin import OptionParser, parser_option
 from itertools import chain
+import ryu.lib.pcaplib as pcap
+from ryu.lib.packet.packet import Packet
+from ryu.lib.packet.bgp import BGPMessage
 
 
 class GoBGPTestBase(unittest.TestCase):
@@ -354,6 +357,37 @@ class GoBGPTestBase(unittest.TestCase):
 
         g1.wait_for(expected_state=BGP_FSM_ESTABLISHED, peer=e1)
 
+    def test_20_check_withdrawal_2(self):
+        g1 = self.gobgp
+        g2 = self.quaggas['g2']
+
+        dumpfile = g2.start_tcpdump()
+
+        g1.add_route('10.40.0.0/24')
+
+        time.sleep(1)
+
+        paths = g2.get_global_rib('10.40.0.0/24')
+        self.assertTrue(len(paths) == 1)
+
+        g1.local('gobgp global rib del 10.40.0.0/24')
+
+        time.sleep(1)
+
+        paths = g2.get_global_rib('10.40.0.0/24')
+        self.assertTrue(len(paths) == 0)
+
+        g2.stop_tcpdump()
+        time.sleep(1)
+
+        cnt = 0
+        for pkt in pcap.Reader(open(dumpfile)):
+            last = Packet(pkt[1]).protocols[-1]
+            if type(last) == str:
+                pkt = BGPMessage.parser(last)[0]
+                cnt += len(pkt.withdrawn_routes)
+
+        self.assertTrue(cnt == 1)
 
 
 if __name__ == '__main__':