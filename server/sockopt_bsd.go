60241ab6ad8c8290d8745388eaed5e6b0efd965f
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2016-08-17 13:29:00 +0900

    server: passive tcp md5 support on OpenBSD
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- server/sockopt_bsd.go
+++ server/sockopt_bsd.go
@@ -12,7 +12,7 @@
 // implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// +build dragonfly freebsd netbsd openbsd
+// +build dragonfly freebsd netbsd
 
 package server
 
