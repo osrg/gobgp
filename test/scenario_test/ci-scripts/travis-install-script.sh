251c32ae0f2658bfa7cefdd22c3aef60c38da1ee
Wataru Ishida <ishida.wataru@lab.ntt.co.jp>
2016-10-03 15:20:55 +0000

    test: add scenario test for zapi v3
    Signed-off-by: Wataru Ishida <ishida.wataru@lab.ntt.co.jp>

--- test/scenario_test/ci-scripts/travis-install-script.sh
+++ test/scenario_test/ci-scripts/travis-install-script.sh
@@ -4,4 +4,4 @@ echo "travis-install-script.sh"
 
 sudo -H pip --quiet install -r test/pip-requires.txt
 
-sudo fab -f test/lib/base.py make_gobgp_ctn:tag=$DOCKER_IMAGE
+sudo fab -f test/lib/base.py make_gobgp_ctn:tag=$DOCKER_IMAGE,from_image=$FROM_IMAGE
