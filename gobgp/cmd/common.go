e913bdc58f8f5b01559c11df87a0bdf17569e867
Chris Stockton <cstockton@godaddy.com>
2016-12-06 17:01:32 -0700

    The rename I mentioned in pull 1183

--- gobgp/cmd/common.go
+++ gobgp/cmd/common.go
@@ -221,7 +221,7 @@ func (v vrfs) Less(i, j int) bool {
 	return v[i].Name < v[j].Name
 }
 
-func newClient() *cli.GoBGPClient {
+func newClient() *cli.Client {
 	target := net.JoinHostPort(globalOpts.Host, strconv.Itoa(globalOpts.Port))
 	client, err := cli.New(target)
 	if err != nil {
