osrg/gobgp

commit c4ced97e76f12d5c37721c107e61147edb4254d7

cli: use client library
Signed-off-by: Wataru Ishida <ishida.wataru@lab.ntt.co.jp>

gobgp/cmd/mrt.go
@@ -17,27 +17,17 @@ package cmd
 
 import (
 	"fmt"
-	api "github.com/osrg/gobgp/api"
-	"github.com/osrg/gobgp/packet/bgp"
-	"github.com/osrg/gobgp/packet/mrt"
-	"github.com/osrg/gobgp/table"
-	"github.com/spf13/cobra"
-	"golang.org/x/net/context"
 	"io"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/osrg/gobgp/packet/mrt"
+	"github.com/osrg/gobgp/table"
+	"github.com/spf13/cobra"
 )
 
-func injectMrt(r string, filename string, count int, skip int, onlyBest bool) error {
-
-	var resource api.Resource
-	switch r {
-	case CMD_GLOBAL:
-		resource = api.Resource_GLOBAL
-	default:
-		return fmt.Errorf("unknown resource type: %s", r)
-	}
+func injectMrt(filename string, count int, skip int, onlyBest bool) error {
 
 	file, err := os.Open(filename)
 	if err != nil {
@@ -46,7 +36,7 @@ func injectMrt(r string, filename string, count int, skip int, onlyBest bool) er
 
 	idx := 0
 
-	ch := make(chan *api.InjectMrtRequest, 1<<20)
+	ch := make(chan []*table.Path, 1<<20)
 
 	go func() {
 
@@ -85,15 +75,10 @@ func injectMrt(r string, filename string, count int, skip int, onlyBest bool) er
 
 			if msg.Header.Type == mrt.TABLE_DUMPv2 {
 				subType := mrt.MRTSubTypeTableDumpv2(msg.Header.SubType)
-				var rf bgp.RouteFamily
 				switch subType {
 				case mrt.PEER_INDEX_TABLE:
 					peers = msg.Body.(*mrt.PeerIndexTable).Peers
 					continue
-				case mrt.RIB_IPV4_UNICAST:
-					rf = bgp.RF_IPv4_UC
-				case mrt.RIB_IPV6_UNICAST:
-					rf = bgp.RF_IPv6_UC
 				default:
 					exitWithError(fmt.Errorf("unsupported subType: %s", subType))
 				}
@@ -105,77 +90,34 @@ func injectMrt(r string, filename string, count int, skip int, onlyBest bool) er
 				rib := msg.Body.(*mrt.Rib)
 				nlri := rib.Prefix
 
-				paths := make([]*api.Path, 0, len(rib.Entries))
+				paths := make([]*table.Path, 0, len(rib.Entries))
 
 				for _, e := range rib.Entries {
 					if len(peers) < int(e.PeerIndex) {
 						exitWithError(fmt.Errorf("invalid peer index: %d (PEER_INDEX_TABLE has only %d peers)\n", e.PeerIndex, len(peers)))
 					}
-
-					if !onlyBest {
-						path := &api.Path{
-							Pattrs:             make([][]byte, 0),
-							NoImplicitWithdraw: true,
-							SourceAsn:          peers[e.PeerIndex].AS,
-							SourceId:           peers[e.PeerIndex].BgpId.String(),
-						}
-
-						if rf == bgp.RF_IPv4_UC {
-							path.Nlri, _ = nlri.Serialize()
-						}
-
-						for _, p := range e.PathAttributes {
-							b, err := p.Serialize()
-							if err != nil {
-								continue
-							}
-							path.Pattrs = append(path.Pattrs, b)
-						}
-
-						paths = append(paths, path)
+					source := &table.PeerInfo{
+						AS: peers[e.PeerIndex].AS,
+						ID: peers[e.PeerIndex].BgpId,
 					}
+					t := time.Unix(int64(e.OriginatedTime), 0)
+					paths = append(paths, table.NewPath(source, nlri, false, e.PathAttributes, t, false))
 				}
-				if onlyBest {
-					paths = func() []*api.Path {
-						dst := table.NewDestination(nlri)
-						pathList := make([]*table.Path, 0, len(rib.Entries))
-						for _, e := range rib.Entries {
-							p := table.NewPath(&table.PeerInfo{AS: peers[e.PeerIndex].AS, ID: peers[e.PeerIndex].BgpId}, nlri, false, e.PathAttributes, time.Unix(int64(e.OriginatedTime), 0), false)
-							dst.AddNewPath(p)
-							pathList = append(pathList, p)
-						}
-						best, _, _ := dst.Calculate([]string{table.GLOBAL_RIB_NAME})
-						for _, p := range pathList {
-							if p == best[table.GLOBAL_RIB_NAME] {
-								nb, _ := nlri.Serialize()
-								return []*api.Path{&api.Path{
-									Nlri:               nb,
-									NoImplicitWithdraw: true,
-									SourceAsn:          p.GetSource().AS,
-									SourceId:           p.GetSource().ID.String(),
-									Pattrs: func() [][]byte {
-										attrs := make([][]byte, 0)
-										for _, a := range p.GetPathAttrs() {
-											if b, e := a.Serialize(); e == nil {
 
-												attrs = append(attrs, b)
-											}
-										}
-										return attrs
-									}(),
-								}}
-							}
-						}
+				if onlyBest {
+					dst := table.NewDestination(nlri)
+					for _, p := range paths {
+						dst.AddNewPath(p)
+					}
+					best, _, _ := dst.Calculate([]string{table.GLOBAL_RIB_NAME})
+					if best[table.GLOBAL_RIB_NAME] == nil {
 						exitWithError(fmt.Errorf("Can't find the best %v", nlri))
-						return []*api.Path{}
-					}()
+					}
+					paths = []*table.Path{best[table.GLOBAL_RIB_NAME]}
 				}
 
 				if idx >= skip {
-					ch <- &api.InjectMrtRequest{
-						Resource: resource,
-						Paths:    paths,
-					}
+					ch <- paths
 				}
 
 				idx += 1
@@ -188,20 +130,19 @@ func injectMrt(r string, filename string, count int, skip int, onlyBest bool) er
 		close(ch)
 	}()
 
-	stream, err := client.InjectMrt(context.Background())
+	stream, err := client.AddPathByStream()
 	if err != nil {
 		return fmt.Errorf("failed to modpath: %s", err)
 	}
 
-	for arg := range ch {
-		err = stream.Send(arg)
+	for paths := range ch {
+		err = stream.Send(paths...)
 		if err != nil {
 			return fmt.Errorf("failed to send: %s", err)
 		}
 	}
 
-	_, err = stream.CloseAndRecv()
-	if err != nil {
+	if err := stream.Close(); err != nil {
 		return fmt.Errorf("failed to send: %s", err)
 	}
 	return nil
@@ -230,7 +171,7 @@ func NewMrtCmd() *cobra.Command {
 					}
 				}
 			}
-			err := injectMrt(CMD_GLOBAL, filename, count, skip, mrtOpts.Best)
+			err := injectMrt(filename, count, skip, mrtOpts.Best)
 			if err != nil {
 				exitWithError(err)
 			}

api/grpc_server.go
@@ -505,7 +505,7 @@ func (s *Server) api2PathList(resource Resource, ApiPathList []*Path) ([]*table.
 			}
 		}
 
-		if nlri == nil || nexthop == "" {
+		if nlri == nil || (!path.IsWithdraw && nexthop == "") {
 			return nil, fmt.Errorf("not found nlri or nexthop")
 		}
 

gobgp/cmd/bmp.go
@@ -17,10 +17,9 @@ package cmd
 
 import (
 	"fmt"
-	api "github.com/osrg/gobgp/api"
+	"github.com/osrg/gobgp/config"
 	"github.com/osrg/gobgp/packet/bmp"
 	"github.com/spf13/cobra"
-	"golang.org/x/net/context"
 	"net"
 	"strconv"
 )
@@ -47,26 +46,24 @@ func modBmpServer(cmdType string, args []string) error {
 	var err error
 	switch cmdType {
 	case CMD_ADD:
-		policyType := api.AddBmpRequest_PRE
+		policyType := config.BMP_ROUTE_MONITORING_POLICY_TYPE_PRE_POLICY
 		if len(args) > 1 {
 			switch args[1] {
-			case "pre":
-				policyType = api.AddBmpRequest_PRE
 			case "post":
-				policyType = api.AddBmpRequest_POST
+				policyType = config.BMP_ROUTE_MONITORING_POLICY_TYPE_POST_POLICY
 			case "both":
-				policyType = api.AddBmpRequest_BOTH
+				policyType = config.BMP_ROUTE_MONITORING_POLICY_TYPE_BOTH
 			default:
 				return fmt.Errorf("invalid bmp policy type. valid type is {pre|post|both}")
 			}
 		}
-		_, err = client.AddBmp(context.Background(), &api.AddBmpRequest{
+		err = client.AddBMP(&config.BmpServerConfig{
 			Address: address,
 			Port:    port,
-			Type:    policyType,
+			RouteMonitoringPolicy: policyType,
 		})
 	case CMD_DEL:
-		_, err = client.DeleteBmp(context.Background(), &api.DeleteBmpRequest{
+		err = client.DeleteBMP(&config.BmpServerConfig{
 			Address: address,
 			Port:    port,
 		})

gobgp/cmd/common.go
@@ -19,15 +19,16 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/osrg/gobgp/api"
-	"github.com/osrg/gobgp/packet/bgp"
-	"google.golang.org/grpc"
 	"net"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
-	"time"
+
+	cli "github.com/osrg/gobgp/client"
+	"github.com/osrg/gobgp/config"
+	"github.com/osrg/gobgp/packet/bgp"
+	"github.com/osrg/gobgp/table"
 )
 
 const (
@@ -162,78 +163,19 @@ func extractReserved(args, keys []string) map[string][]string {
 	return m
 }
 
-type PeerConf struct {
-	RemoteIp          string                             `json:"remote_ip,omitempty"`
-	Id                net.IP                             `json:"id,omitempty"`
-	RemoteAs          uint32                             `json:"remote_as,omitempty"`
-	LocalAs           uint32                             `json:"local-as,omitempty"`
-	RemoteCap         []bgp.ParameterCapabilityInterface `json:"remote_cap,omitempty"`
-	LocalCap          []bgp.ParameterCapabilityInterface `json:"local_cap,omitempty"`
-	Holdtime          uint32                             `json:"holdtime,omitempty"`
-	KeepaliveInterval uint32                             `json:"keepalive_interval,omitempty"`
-	PrefixLimits      []*gobgpapi.PrefixLimit            `json:"prefix_limits,omitempty"`
-	LocalIp           string                             `json:"local_ip,omitempty"`
-	Interface         string                             `json:"interface,omitempty"`
-	Description       string                             `json:"description,omitempty"`
-	VRF               string                             `json:"vrf,omitempty"`
+type neighbors []*config.Neighbor
+
+func (n neighbors) Len() int {
+	return len(n)
 }
 
-type Peer struct {
-	Conf           PeerConf                 `json:"conf,omitempty"`
-	Info           *gobgpapi.PeerState      `json:"info,omitempty"`
-	Timers         *gobgpapi.Timers         `json:"timers,omitempty"`
-	RouteReflector *gobgpapi.RouteReflector `json:"route_reflector,omitempty"`
-	RouteServer    *gobgpapi.RouteServer    `json:"route_server,omitempty"`
+func (n neighbors) Swap(i, j int) {
+	n[i], n[j] = n[j], n[i]
 }
 
-func ApiStruct2Peer(p *gobgpapi.Peer) *Peer {
-	localCaps := capabilities{}
-	remoteCaps := capabilities{}
-	for _, buf := range p.Conf.LocalCap {
-		c, _ := bgp.DecodeCapability(buf)
-		localCaps = append(localCaps, c)
-	}
-	for _, buf := range p.Conf.RemoteCap {
-		c, _ := bgp.DecodeCapability(buf)
-		remoteCaps = append(remoteCaps, c)
-	}
-	remoteIp, _ := net.ResolveIPAddr("ip", p.Conf.NeighborAddress)
-	localIp, _ := net.ResolveIPAddr("ip", p.Conf.LocalAddress)
-	conf := PeerConf{
-		RemoteIp:     remoteIp.String(),
-		Id:           net.ParseIP(p.Conf.Id),
-		RemoteAs:     p.Conf.PeerAs,
-		LocalAs:      p.Conf.LocalAs,
-		RemoteCap:    remoteCaps,
-		LocalCap:     localCaps,
-		PrefixLimits: p.Conf.PrefixLimits,
-		LocalIp:      localIp.String(),
-		Interface:    p.Conf.NeighborInterface,
-		Description:  p.Conf.Description,
-		VRF:          p.Conf.Vrf,
-	}
-	return &Peer{
-		Conf:           conf,
-		Info:           p.Info,
-		Timers:         p.Timers,
-		RouteReflector: p.RouteReflector,
-		RouteServer:    p.RouteServer,
-	}
-}
-
-type peers []*Peer
-
-func (p peers) Len() int {
-	return len(p)
-}
-
-func (p peers) Swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
-}
-
-func (p peers) Less(i, j int) bool {
-	p1 := p[i].Conf.RemoteIp
-	p2 := p[j].Conf.RemoteIp
+func (n neighbors) Less(i, j int) bool {
+	p1 := n[i].Config.NeighborAddress
+	p2 := n[j].Config.NeighborAddress
 	p1Isv4 := !strings.Contains(p1, ":")
 	p2Isv4 := !strings.Contains(p2, ":")
 	if p1Isv4 != p2Isv4 {
@@ -265,51 +207,7 @@ func (c capabilities) Less(i, j int) bool {
 	return c[i].Code() < c[j].Code()
 }
 
-type sets []*gobgpapi.DefinedSet
-
-func (n sets) Len() int {
-	return len(n)
-}
-
-func (n sets) Swap(i, j int) {
-	n[i], n[j] = n[j], n[i]
-}
-
-func (n sets) Less(i, j int) bool {
-	return n[i].Name < n[j].Name
-}
-
-type policies []*gobgpapi.Policy
-
-func (p policies) Len() int {
-	return len(p)
-}
-
-func (p policies) Swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
-}
-
-func (p policies) Less(i, j int) bool {
-	return p[i].Name < p[j].Name
-}
-
-type roas []*gobgpapi.Roa
-
-func (r roas) Len() int {
-	return len(r)
-}
-
-func (r roas) Swap(i, j int) {
-	r[i], r[j] = r[j], r[i]
-}
-
-func (r roas) Less(i, j int) bool {
-	strings := sort.StringSlice{cidr2prefix(fmt.Sprintf("%s/%d", r[i].Prefix, r[i].Prefixlen)),
-		cidr2prefix(fmt.Sprintf("%s/%d", r[j].Prefix, r[j].Prefixlen))}
-	return strings.Less(0, 1)
-}
-
-type vrfs []*gobgpapi.Vrf
+type vrfs []*table.Vrf
 
 func (v vrfs) Len() int {
 	return len(v)
@@ -323,14 +221,13 @@ func (v vrfs) Less(i, j int) bool {
 	return v[i].Name < v[j].Name
 }
 
-func connGrpc() *grpc.ClientConn {
-	timeout := grpc.WithTimeout(time.Second)
+func newClient() *cli.GoBGPClient {
 	target := net.JoinHostPort(globalOpts.Host, strconv.Itoa(globalOpts.Port))
-	conn, err := grpc.Dial(target, timeout, grpc.WithBlock(), grpc.WithInsecure())
+	client, err := cli.NewGoBGPClient(target)
 	if err != nil {
 		exitWithError(err)
 	}
-	return conn
+	return client
 }
 
 func addr2AddressFamily(a net.IP) bgp.RouteFamily {

gobgp/cmd/global.go
@@ -18,16 +18,17 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	api "github.com/osrg/gobgp/api"
-	"github.com/osrg/gobgp/packet/bgp"
-	"github.com/osrg/gobgp/table"
-	"github.com/spf13/cobra"
-	"golang.org/x/net/context"
 	"net"
 	"regexp"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/osrg/gobgp/config"
+	"github.com/osrg/gobgp/packet/bgp"
+	"github.com/osrg/gobgp/table"
+	"github.com/spf13/cobra"
 )
 
 type ExtCommType int
@@ -655,16 +656,12 @@ func extractRouteDistinguisher(args []string) ([]string, bgp.RouteDistinguisherI
 	return args, nil, nil
 }
 
-func ParsePath(rf bgp.RouteFamily, args []string) (*api.Path, error) {
+func ParsePath(rf bgp.RouteFamily, args []string) (*table.Path, error) {
 	var nlri bgp.AddrPrefixInterface
 	var rd bgp.RouteDistinguisherInterface
 	var extcomms []string
 	var err error
 	attrs := table.PathAttrs(make([]bgp.PathAttributeInterface, 0, 1))
-
-	path := &api.Path{
-		Pattrs: make([][]byte, 0),
-	}
 
 	fns := []func([]string) ([]string, bgp.PathAttributeInterface, error){
 		extractOrigin,
@@ -801,7 +798,6 @@ func ParsePath(rf bgp.RouteFamily, args []string) (*api.Path, error) {
 	}
 
 	if rf == bgp.RF_IPv4_UC {
-		path.Nlri, _ = nlri.Serialize()
 		attrs = append(attrs, bgp.NewPathAttributeNextHop(nexthop))
 	} else {
 		mpreach := bgp.NewPathAttributeMpReachNLRI(nexthop, []bgp.AddrPrefixInterface{nlri})
@@ -819,21 +815,14 @@ func ParsePath(rf bgp.RouteFamily, args []string) (*api.Path, error) {
 
 	sort.Sort(attrs)
 
-	for _, attr := range attrs {
-		buf, err := attr.Serialize()
-		if err != nil {
-			return nil, err
-		}
-		path.Pattrs = append(path.Pattrs, buf)
-	}
-	return path, nil
+	return table.NewPath(nil, nlri, false, attrs, time.Now(), false), nil
 }
 
 func showGlobalRib(args []string) error {
 	return showNeighborRib(CMD_GLOBAL, "", args)
 }
 
-func modPath(resource api.Resource, name, modtype string, args []string) error {
+func modPath(resource string, name, modtype string, args []string) error {
 	rf, err := checkAddressFamily(bgp.RF_IPv4_UC)
 	if err != nil {
 		return err
@@ -842,7 +831,7 @@ func modPath(resource api.Resource, name, modtype string, args []string) error {
 	path, err := ParsePath(rf, args)
 	if err != nil {
 		cmdstr := "global"
-		if resource == api.Resource_VRF {
+		if resource == CMD_VRF {
 			cmdstr = fmt.Sprintf("vrf %s", name)
 		}
 
@@ -929,40 +918,37 @@ usage: %s rib %s%%smatch <MATCH_EXPR> then <THEN_EXPR> -a %%s
 	}
 
 	if modtype == CMD_ADD {
-		arg := &api.AddPathRequest{
-			Resource: resource,
-			VrfId:    name,
-			Path:     path,
+		if resource == CMD_VRF {
+			_, err = client.AddVRFPath(name, []*table.Path{path})
+		} else {
+			_, err = client.AddPath([]*table.Path{path})
 		}
-		_, err = client.AddPath(context.Background(), arg)
 	} else {
-		arg := &api.DeletePathRequest{
-			Resource: resource,
-			VrfId:    name,
-			Path:     path,
+		if resource == CMD_VRF {
+			err = client.DeleteVRFPath(name, []*table.Path{path})
+		} else {
+			err = client.DeletePath([]*table.Path{path})
 		}
-		_, err = client.DeletePath(context.Background(), arg)
 	}
 	return err
 }
 
 func showGlobalConfig(args []string) error {
-	rsp, err := client.GetServer(context.Background(), &api.GetServerRequest{})
+	g, err := client.GetServer()
 	if err != nil {
 		return err
 	}
-	g := rsp.Global
 	if globalOpts.Json {
 		j, _ := json.Marshal(g)
 		fmt.Println(string(j))
 		return nil
 	}
-	fmt.Println("AS:       ", g.As)
-	fmt.Println("Router-ID:", g.RouterId)
-	if len(g.ListenAddresses) > 0 {
-		fmt.Printf("Listening Port: %d, Addresses: %s\n", g.ListenPort, strings.Join(g.ListenAddresses, ", "))
+	fmt.Println("AS:       ", g.Config.As)
+	fmt.Println("Router-ID:", g.Config.RouterId)
+	if len(g.Config.LocalAddressList) > 0 {
+		fmt.Printf("Listening Port: %d, Addresses: %s\n", g.Config.Port, strings.Join(g.Config.LocalAddressList, ", "))
 	}
-	if g.UseMultiplePaths {
+	if g.UseMultiplePaths.Config.Enabled {
 		fmt.Printf("Multipath: enabled")
 	}
 	return nil
@@ -994,16 +980,19 @@ func modGlobalConfig(args []string) error {
 	if _, ok := m["use-multipath"]; ok {
 		useMultipath = true
 	}
-	_, err = client.StartServer(context.Background(), &api.StartServerRequest{
-		Global: &api.Global{
+	return client.StartServer(&config.Global{
+		Config: config.GlobalConfig{
 			As:               uint32(asn),
 			RouterId:         id.String(),
-			ListenPort:       int32(port),
-			ListenAddresses:  m["listen-addresses"],
-			UseMultiplePaths: useMultipath,
+			Port:             int32(port),
+			LocalAddressList: m["listen-addresses"],
+		},
+		UseMultiplePaths: config.UseMultiplePaths{
+			Config: config.UseMultiplePathsConfig{
+				Enabled: useMultipath,
+			},
 		},
 	})
-	return err
 }
 
 func NewGlobalCmd() *cobra.Command {
@@ -1037,7 +1026,7 @@ func NewGlobalCmd() *cobra.Command {
 		cmd := &cobra.Command{
 			Use: v,
 			Run: func(cmd *cobra.Command, args []string) {
-				err := modPath(api.Resource_GLOBAL, "", cmd.Use, args)
+				err := modPath(CMD_GLOBAL, "", cmd.Use, args)
 				if err != nil {
 					exitWithError(err)
 				}
@@ -1053,12 +1042,7 @@ func NewGlobalCmd() *cobra.Command {
 					if err != nil {
 						exitWithError(err)
 					}
-					arg := &api.DeletePathRequest{
-						Resource: api.Resource_GLOBAL,
-						Family:   uint32(family),
-					}
-					_, err = client.DeletePath(context.Background(), arg)
-					if err != nil {
+					if err = client.DeletePathByFamily(family); err != nil {
 						exitWithError(err)
 					}
 				},
@@ -1124,8 +1108,7 @@ func NewGlobalCmd() *cobra.Command {
 	allCmd := &cobra.Command{
 		Use: CMD_ALL,
 		Run: func(cmd *cobra.Command, args []string) {
-			_, err := client.StopServer(context.Background(), &api.StopServerRequest{})
-			if err != nil {
+			if err := client.StopServer(); err != nil {
 				exitWithError(err)
 			}
 		},

gobgp/cmd/global_test.go
@@ -30,11 +30,8 @@ func Test_ParsePath(t *testing.T) {
 	path, err := ParsePath(bgp.RF_IPv4_UC, strings.Split(buf, " "))
 	assert.Nil(err)
 	fmt.Println(path)
-	path.Family = uint32(bgp.RF_IPv4_UC)
-	p, err := path.ToNativePath()
-	assert.Nil(err)
 	i := 0
-	for _, a := range p.GetPathAttrs() {
+	for _, a := range path.GetPathAttrs() {
 		assert.True(i < int(a.GetType()))
 		i = int(a.GetType())
 	}

gobgp/cmd/monitor.go
@@ -18,33 +18,26 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/osrg/gobgp/api"
-	"github.com/osrg/gobgp/packet/bgp"
-	"github.com/spf13/cobra"
-	"golang.org/x/net/context"
 	"io"
 	"net"
+
+	"github.com/osrg/gobgp/packet/bgp"
+	"github.com/osrg/gobgp/table"
+	"github.com/spf13/cobra"
 )
 
 func NewMonitorCmd() *cobra.Command {
 
-	monitor := func(arg *gobgpapi.Table) {
-		stream, err := client.MonitorRib(context.Background(), arg)
-		if err != nil {
-			exitWithError(err)
-		}
+	monitor := func(recver interface {
+		Recv() (*table.Destination, error)
+	}) {
 		for {
-			d, err := stream.Recv()
+			dst, err := recver.Recv()
 			if err == io.EOF {
 				break
 			} else if err != nil {
 				exitWithError(err)
 			}
-			dst, err := d.ToNativeDestination()
-			if err != nil {
-				exitWithError(err)
-			}
-
 			if globalOpts.Json {
 				j, _ := json.Marshal(dst.GetAllKnownPathList())
 				fmt.Println(string(j))
@@ -61,11 +54,11 @@ func NewMonitorCmd() *cobra.Command {
 			if err != nil {
 				exitWithError(err)
 			}
-			arg := &gobgpapi.Table{
-				Type:   gobgpapi.Resource_GLOBAL,
-				Family: uint32(family),
+			recver, err := client.MonitorRIB(family)
+			if err != nil {
+				exitWithError(err)
 			}
-			monitor(arg)
+			monitor(recver)
 		},
 	}
 	ribCmd.PersistentFlags().StringVarP(&subOpts.AddressFamily, "address-family", "a", "", "address family")
@@ -78,16 +71,11 @@ func NewMonitorCmd() *cobra.Command {
 	neighborCmd := &cobra.Command{
 		Use: CMD_NEIGHBOR,
 		Run: func(cmd *cobra.Command, args []string) {
-			var arg *gobgpapi.Arguments
+			var names []string
 			if len(args) > 0 {
-				arg = &gobgpapi.Arguments{
-					Name: args[0],
-				}
-			} else {
-				arg = &gobgpapi.Arguments{}
+				names = []string{args[0]}
 			}
-
-			stream, err := client.MonitorPeerState(context.Background(), arg)
+			stream, err := client.MonitorNeighborState(names...)
 			if err != nil {
 				exitWithError(err)
 			}
@@ -102,7 +90,7 @@ func NewMonitorCmd() *cobra.Command {
 					j, _ := json.Marshal(s)
 					fmt.Println(string(j))
 				} else {
-					fmt.Printf("[NEIGH] %s fsm: %s admin: %s\n", s.Conf.NeighborAddress, s.Info.BgpState, s.Info.AdminState)
+					fmt.Printf("[NEIGH] %s fsm: %s admin: %s\n", s.Config.NeighborAddress, s.State.SessionState, s.State.AdminState)
 				}
 			}
 		},
@@ -123,12 +111,11 @@ func NewMonitorCmd() *cobra.Command {
 			if err != nil {
 				exitWithError(err)
 			}
-			arg := &gobgpapi.Table{
-				Type:   gobgpapi.Resource_ADJ_IN,
-				Family: uint32(family),
-				Name:   name,
+			recver, err := client.MonitorAdjRIBIn(name, family)
+			if err != nil {
+				exitWithError(err)
 			}
-			monitor(arg)
+			monitor(recver)
 		},
 	}
 	adjInCmd.PersistentFlags().StringVarP(&subOpts.AddressFamily, "address-family", "a", "", "address family")

commit 51548046cdd3bd128cf018a706fcbf5cfb2fe3ce

cli: fix bug of injecting mrt table dump v2 data
a bug introduced by c4ced97e76f12d5c37721c107e61147edb4254d7

Signed-off-by: Wataru Ishida <ishida.wataru@lab.ntt.co.jp>

gobgp/cmd/mrt.go
@@ -79,6 +79,7 @@ func injectMrt(filename string, count int, skip int, onlyBest bool) error {
 				case mrt.PEER_INDEX_TABLE:
 					peers = msg.Body.(*mrt.PeerIndexTable).Peers
 					continue
+				case mrt.RIB_IPV4_UNICAST, mrt.RIB_IPV6_UNICAST:
 				default:
 					exitWithError(fmt.Errorf("unsupported subType: %v", subType))
 				}