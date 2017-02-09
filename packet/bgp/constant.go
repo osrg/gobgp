1e7f93a4e8cc0ffc687d1aa447b94cd57beb50ac
Matthieu Texier <matthieu@texier.tv>
2017-02-09 11:45:50 +0100

    fixing go fmt issue and flowspec test scenario

--- packet/bgp/constant.go
+++ packet/bgp/constant.go
@@ -96,67 +96,67 @@ func (p Protocol) String() string {
 type TCPFlag int
 
 const (
-    TCP_FLAG_FIN    = 0x01
-    TCP_FLAG_SYN    = 0x02
-    TCP_FLAG_RST    = 0x04
-    TCP_FLAG_PUSH   = 0x08
-    TCP_FLAG_ACK    = 0x10
-    TCP_FLAG_URGENT = 0x20
-    TCP_FLAG_CWR    = 0x40
-    TCP_FLAG_ECE    = 0x80
+	TCP_FLAG_FIN    = 0x01
+	TCP_FLAG_SYN    = 0x02
+	TCP_FLAG_RST    = 0x04
+	TCP_FLAG_PUSH   = 0x08
+	TCP_FLAG_ACK    = 0x10
+	TCP_FLAG_URGENT = 0x20
+	TCP_FLAG_CWR    = 0x40
+	TCP_FLAG_ECE    = 0x80
 )
 
 var TCPFlagNameMap = map[TCPFlag]string{
-    TCP_FLAG_FIN:    "F",
-    TCP_FLAG_SYN:    "S",
-    TCP_FLAG_RST:    "R",
-    TCP_FLAG_PUSH:   "P",
-    TCP_FLAG_ACK:    "A",
-    TCP_FLAG_URGENT: "U",
-    TCP_FLAG_CWR:    "C",
-    TCP_FLAG_ECE :   "E",
+	TCP_FLAG_FIN:    "F",
+	TCP_FLAG_SYN:    "S",
+	TCP_FLAG_RST:    "R",
+	TCP_FLAG_PUSH:   "P",
+	TCP_FLAG_ACK:    "A",
+	TCP_FLAG_URGENT: "U",
+	TCP_FLAG_CWR:    "C",
+	TCP_FLAG_ECE:    "E",
 }
 
 var TCPFlagValueMap = map[string]TCPFlag{
-    TCPFlagNameMap[TCP_FLAG_FIN]:    TCP_FLAG_FIN,
-    TCPFlagNameMap[TCP_FLAG_SYN]:    TCP_FLAG_SYN,
-    TCPFlagNameMap[TCP_FLAG_RST]:    TCP_FLAG_RST,
-    TCPFlagNameMap[TCP_FLAG_PUSH]:   TCP_FLAG_PUSH,
-    TCPFlagNameMap[TCP_FLAG_ACK]:    TCP_FLAG_ACK,
-    TCPFlagNameMap[TCP_FLAG_URGENT]: TCP_FLAG_URGENT,
-    TCPFlagNameMap[TCP_FLAG_CWR]:    TCP_FLAG_CWR,
-    TCPFlagNameMap[TCP_FLAG_ECE]:    TCP_FLAG_ECE,
+	TCPFlagNameMap[TCP_FLAG_FIN]:    TCP_FLAG_FIN,
+	TCPFlagNameMap[TCP_FLAG_SYN]:    TCP_FLAG_SYN,
+	TCPFlagNameMap[TCP_FLAG_RST]:    TCP_FLAG_RST,
+	TCPFlagNameMap[TCP_FLAG_PUSH]:   TCP_FLAG_PUSH,
+	TCPFlagNameMap[TCP_FLAG_ACK]:    TCP_FLAG_ACK,
+	TCPFlagNameMap[TCP_FLAG_URGENT]: TCP_FLAG_URGENT,
+	TCPFlagNameMap[TCP_FLAG_CWR]:    TCP_FLAG_CWR,
+	TCPFlagNameMap[TCP_FLAG_ECE]:    TCP_FLAG_ECE,
 }
 
 type TCPFlagOp int
 
 const (
-    TCP_FLAG_OP_OR      = 0x00
-    TCP_FLAG_OP_AND     = 0x40
-    TCP_FLAG_OP_END     = 0x80
-    TCP_FLAG_OP_NOT     = 0x02
-    TCP_FLAG_OP_MATCH   = 0x01
+	TCP_FLAG_OP_OR    = 0x00
+	TCP_FLAG_OP_AND   = 0x40
+	TCP_FLAG_OP_END   = 0x80
+	TCP_FLAG_OP_NOT   = 0x02
+	TCP_FLAG_OP_MATCH = 0x01
 )
 
 var TCPFlagOpNameMap = map[TCPFlagOp]string{
-    TCP_FLAG_OP_OR:     " ",
-    TCP_FLAG_OP_AND:    "&",
-    TCP_FLAG_OP_END:    "E",
-    TCP_FLAG_OP_NOT:    "!",
-    TCP_FLAG_OP_MATCH:  "=",
+	TCP_FLAG_OP_OR:    " ",
+	TCP_FLAG_OP_AND:   "&",
+	TCP_FLAG_OP_END:   "E",
+	TCP_FLAG_OP_NOT:   "!",
+	TCP_FLAG_OP_MATCH: "=",
 }
 
 var TCPFlagOpValueMap = map[string]TCPFlagOp{
-    TCPFlagOpNameMap[TCP_FLAG_OP_OR]:       TCP_FLAG_OP_OR,
-    TCPFlagOpNameMap[TCP_FLAG_OP_AND]:      TCP_FLAG_OP_AND,
-    TCPFlagOpNameMap[TCP_FLAG_OP_END]:      TCP_FLAG_OP_END,
-    TCPFlagOpNameMap[TCP_FLAG_OP_NOT]:      TCP_FLAG_OP_NOT,
-    TCPFlagOpNameMap[TCP_FLAG_OP_MATCH]:    TCP_FLAG_OP_MATCH,
+	TCPFlagOpNameMap[TCP_FLAG_OP_OR]:    TCP_FLAG_OP_OR,
+	TCPFlagOpNameMap[TCP_FLAG_OP_AND]:   TCP_FLAG_OP_AND,
+	TCPFlagOpNameMap[TCP_FLAG_OP_END]:   TCP_FLAG_OP_END,
+	TCPFlagOpNameMap[TCP_FLAG_OP_NOT]:   TCP_FLAG_OP_NOT,
+	TCPFlagOpNameMap[TCP_FLAG_OP_MATCH]: TCP_FLAG_OP_MATCH,
 }
 
 func (f TCPFlag) String() string {
 	ss := make([]string, 0, 6)
-	for _, v := range []TCPFlag{TCP_FLAG_FIN, TCP_FLAG_SYN, TCP_FLAG_RST, TCP_FLAG_PUSH, TCP_FLAG_ACK, TCP_FLAG_URGENT, TCP_FLAG_CWR, TCP_FLAG_ECE, } {
+	for _, v := range []TCPFlag{TCP_FLAG_FIN, TCP_FLAG_SYN, TCP_FLAG_RST, TCP_FLAG_PUSH, TCP_FLAG_ACK, TCP_FLAG_URGENT, TCP_FLAG_CWR, TCP_FLAG_ECE} {
 		if f&v > 0 {
 			ss = append(ss, TCPFlagNameMap[v])
 		}
