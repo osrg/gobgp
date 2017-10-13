ec31dbe5f3b4122057f639169c2baba6d34f0264
IWASE Yusuke <iwase.yusuke0@gmail.com>
2017-10-13 14:33:41 +0900

    travis: Update .travis.yml
    For scenario_test, Python build environment is more suitable rather than
    Go build environment, because scenario_test is written in Python and the
    building source codes will be done on Docker containers.
    
    This patch fixes scenario_test to use Python build environment and
    reduces "sudo" executions in order to use interpreter of virtualenv.
    
    Note: On Travis-Ci, the Python interpreter of sudoer is older version,
    and when install packages with "sudo pip", InsecurePlatformWarning will
    be displayed. So it is better to use the newer interpreter of virtualenv.
    
    Signed-off-by: IWASE Yusuke <iwase.yusuke0@gmail.com>

--- test/scenario_test/ci-scripts/travis-build-script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-echo "travis-build-script.sh"
-
-sudo PYTHONPATH=test python test/scenario_test/$TEST --gobgp-image $DOCKER_IMAGE -x -s
--- test/scenario_test/ci-scripts/travis-install-script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-echo "travis-install-script.sh"
-
-sudo -H pip --quiet install -r test/pip-requires.txt
-
-sudo fab -f test/lib/base.py make_gobgp_ctn:tag=$DOCKER_IMAGE,from_image=$FROM_IMAGE
