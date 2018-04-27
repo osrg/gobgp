5cb07585d900417bed029ee1adc09feba2564ee6
Aman Shaikh <ashaikh@research.att.com>
2018-04-27 15:02:07 -0400

    Add debug message for label encoding

--- zebra/zapi.go
+++ zebra/zapi.go
@@ -2078,6 +2078,13 @@ func serializeNexthops(nexthops []*Nexthop, addLabels bool, version uint8) ([]by
 				bbuf[2] = uint8((label & 0x000000FF) << 12)
 				bbuf[1] = uint8((label & 0x0000FF00) << 4)
 				bbuf[0] = uint8((label & 0x00FF0000) >> 4)
+				log.WithFields(log.Fields{
+					"Topic":   "Zebra",
+					"bbuf[0]": bbuf[0],
+					"bbuf[1]": bbuf[1],
+					"bbuf[2]": bbuf[2],
+					"bbuf[3]": bbuf[3],
+				})
 				buf = append(buf, bbuf...)
 			}
 		}
