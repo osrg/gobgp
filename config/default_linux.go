eb7816a00a9252b17594d99b3c36dbba0ff0613b
Paul Greenberg <greenpau@outlook.com>
2018-05-29 05:38:06 -0400

    config: remove dependency on external netlink library
    This commit removes the dependency on the external netlink library
    "github.com/vishvananda/netlink" with stdlib syscall.
    
    Resolves: #1702
    
    Signed-off-by: Paul Greenberg <greenpau@outlook.com>

--- config/default_linux.go
+++ config/default_linux.go
@@ -18,37 +18,83 @@ package config
 
 import (
 	"fmt"
-	"github.com/vishvananda/netlink"
 	"net"
+	"syscall"
+	"unsafe"
 )
 
+// For reference, see http://man7.org/linux/man-pages/man7/rtnetlink.7.html
+type ndmsg struct {
+	Family  uint8
+	IfIndex uint32
+	State   uint16
+	Flags   uint8
+	Type    uint8
+}
+
 func GetIPv6LinkLocalNeighborAddress(ifname string) (string, error) {
+	ipAddrs := []net.IP{}
 	ifi, err := net.InterfaceByName(ifname)
 	if err != nil {
 		return "", err
 	}
-	neighs, err := netlink.NeighList(ifi.Index, netlink.FAMILY_V6)
+	rib, err := syscall.NetlinkRIB(syscall.RTM_GETNEIGH, int(syscall.AF_INET6))
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("netlink: get IPv6 RIB: %s", err)
 	}
-	cnt := 0
-	var addr net.IP
-	for _, neigh := range neighs {
-		local, err := isLocalLinkLocalAddress(ifi.Index, neigh.IP)
-		if err != nil {
-			return "", err
+	messages, err := syscall.ParseNetlinkMessage(rib)
+	if err != nil {
+		return "", fmt.Errorf("netlink: parse IPv6 RIB: %s", err)
+	}
+	for _, m := range messages {
+		if m.Header.Type != syscall.RTM_NEWNEIGH {
+			continue
 		}
-		if neigh.State&netlink.NUD_FAILED == 0 && neigh.IP.IsLinkLocalUnicast() && !local {
-			addr = neigh.IP
-			cnt += 1
+		data := m.Data[12:]
+		routeAttrs := []syscall.NetlinkRouteAttr{}
+		for len(data) >= syscall.SizeofRtAttr {
+			parseRouteAttr := func(data []byte) (*syscall.RtAttr, []byte, int, error) {
+				dataLen := len(data)
+				attr := (*syscall.RtAttr)(unsafe.Pointer(&data[0]))
+				attrLen := int(attr.Len)
+				if attrLen < syscall.SizeofRtAttr || attrLen > dataLen {
+					return nil, nil, 0, syscall.EINVAL
+				}
+				align := (attrLen + syscall.RTA_ALIGNTO - 1) & ^(syscall.RTA_ALIGNTO - 1)
+				return attr, data[syscall.SizeofRtAttr:], align, nil
+			}
+			attr, attrBuffer, attrLen, err := parseRouteAttr(data)
+			if err != nil {
+				break
+			}
+			attrValue := attrBuffer[:int(attr.Len)-syscall.SizeofRtAttr]
+			routeAttr := syscall.NetlinkRouteAttr{Attr: *attr, Value: attrValue}
+			routeAttrs = append(routeAttrs, routeAttr)
+			data = data[attrLen:]
+		}
+		if len(routeAttrs) == 0 {
+			continue
+		}
+		for _, attr := range routeAttrs {
+			if attr.Attr.Type == 1 {
+				ndm := (*ndmsg)(unsafe.Pointer(&m.Data[0]))
+				ipAddr := net.IP(attr.Value)
+				local, err := isLocalLinkLocalAddress(ifi.Index, ipAddr)
+				if err != nil {
+					continue
+				}
+				if ndm.State&0x20 == 0 && ipAddr.IsLinkLocalUnicast() && !local {
+					ipAddrs = append(ipAddrs, ipAddr)
+				}
+			}
 		}
 	}
-
-	if cnt == 0 {
+	switch len(ipAddrs) {
+	case 1:
+		return fmt.Sprintf("%s%%%s", ipAddrs[0], ifname), nil
+	case 0:
 		return "", fmt.Errorf("no ipv6 link-local neighbor found")
-	} else if cnt > 1 {
-		return "", fmt.Errorf("found %d link-local neighbors. only support p2p link", cnt)
+	default:
+		return "", fmt.Errorf("found %d link-local neighbors. only support p2p link", len(ipAddrs))
 	}
-
-	return fmt.Sprintf("%s%%%s", addr, ifname), nil
 }
