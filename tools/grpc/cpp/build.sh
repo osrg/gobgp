1cf93d5acfd9ed16ad798c98142291bdb7688d84
tamihiro <dead6oy@gmail.com>
2018-07-19 18:40:53 +0900

    fix path to lib directory

--- tools/grpc/cpp/build.sh
+++ tools/grpc/cpp/build.sh
@@ -2,10 +2,10 @@
 
 GOBGP_PATH=${GOPATH}/src/github.com/osrg/gobgp
 
-cd ${GOBGP_PATH}/gobgp/lib
+cd ${GOBGP_PATH}/cmd/gobgp/lib
 go build -buildmode=c-shared -o libgobgp.so *.go
 cd ${GOBGP_PATH}/tools/grpc/cpp
-ln -s ${GOBGP_PATH}/gobgp/lib/libgobgp.h
-ln -s ${GOBGP_PATH}/gobgp/lib/libgobgp.so
-ln -s ${GOBGP_PATH}/api/gobgp.proto gobgp_api_client.proto
+ln -sf ${GOBGP_PATH}/cmd/gobgp/lib/libgobgp.h
+ln -sf ${GOBGP_PATH}/cmd/gobgp/lib/libgobgp.so
+ln -sf ${GOBGP_PATH}/api/gobgp.proto gobgp_api_client.proto
 make
