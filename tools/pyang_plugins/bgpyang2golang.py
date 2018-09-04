70d3ddfd662d9c100b37b9875de1c0c256699931
IWASE Yusuke <iwase.yusuke0@gmail.com>
2018-09-04 22:48:22 +0900

    pyang_plugins: Fix an import path
    Signed-off-by: IWASE Yusuke <iwase.yusuke0@gmail.com>

--- tools/pyang_plugins/bgpyang2golang.py
+++ tools/pyang_plugins/bgpyang2golang.py
@@ -768,7 +768,7 @@ def generate_header(fd):
     print('import (', file=fd)
     print('"fmt"', file=fd)
     print('', file=fd)
-    print('"github.com/osrg/gobgp/packet/bgp"', file=fd)
+    print('"github.com/osrg/gobgp/pkg/packet/bgp"', file=fd)
     print(')', file=fd)
     print('', file=fd)
 
