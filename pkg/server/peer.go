osrg/gobgp

commit 433440067d8084556a31ffd4b8bfa8671bfcaab2

server: fix isIBGPPeer() to handle local-as configuration
Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

pkg/server/peer.go
@@ -140,7 +140,9 @@ func (peer *peer) TableID() string {
 }
 
 func (peer *peer) isIBGPPeer() bool {
-	return peer.fsm.pConf.State.PeerAs == peer.fsm.gConf.Config.As
+	peer.fsm.lock.RLock()
+	defer peer.fsm.lock.RUnlock()
+	return peer.fsm.pConf.State.PeerType == config.PEER_TYPE_INTERNAL
 }
 
 func (peer *peer) isRouteServerClient() bool {

commit 061c94699775397a1a970d698f8ce42c3145441e

server: fix deadlock regression
introduced by 433440067d8084556a31ffd4b8bfa8671bfcaab2

Reported-by: Eiichiro Watanabe <a16tochjp@gmail.com>
Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

pkg/server/peer.go
@@ -497,8 +497,8 @@ func (peer *peer) handleUpdate(e *fsmMsg) ([]*table.Path, []bgp.RouteFamily, *bg
 			// RFC4456 8. Avoiding Routing Information Loops
 			// A router that recognizes the ORIGINATOR_ID attribute SHOULD
 			// ignore a route received with its BGP Identifier as the ORIGINATOR_ID.
-			peer.fsm.lock.RLock()
 			isIBGPPeer := peer.isIBGPPeer()
+			peer.fsm.lock.RLock()
 			routerId := peer.fsm.gConf.Config.RouterId
 			peer.fsm.lock.RUnlock()
 			if isIBGPPeer {