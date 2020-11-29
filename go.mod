934f59aa3b822b4532520dcd5c0af8901bc92396
Serguei Bezverkhi <sbezverk@cisco.com>
2020-11-29 10:07:59 -0500

    Unit tests and dependencies
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- go.mod
+++ go.mod
@@ -3,23 +3,25 @@ module github.com/osrg/gobgp
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/coreos/go-systemd v0.0.0-20190321100706-95778dfbb74e
-	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-farm v0.0.0-20171119141306-ac7624ea8da3
 	github.com/eapache/channels v1.1.0
-	github.com/eapache/queue v1.0.2 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.2 // indirect
+	github.com/go-test/deep v1.0.6
 	github.com/golang/protobuf v1.0.0
 	github.com/google/uuid v1.1.1
 	github.com/hashicorp/hcl v0.0.0-20170509225359-392dba7d905e // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jessevdk/go-flags v1.3.0
 	github.com/k-sone/critbitgo v1.3.1-0.20191024122315-48c9e1530131
-	github.com/kr/pretty v0.1.0
+	github.com/kr/pretty v0.2.0
+	github.com/kr/text v0.2.0 // indirect
 	github.com/magiconair/properties v1.7.3 // indirect
 	github.com/mitchellh/mapstructure v0.0.0-20170523030023-d0303fe80992 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pelletier/go-buffruneio v0.2.0 // indirect
 	github.com/pelletier/go-toml v1.0.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v0.0.0-20170713114250-a3f95b5c4235
 	github.com/spf13/afero v0.0.0-20170217164146-9be650865eab // indirect
 	github.com/spf13/cast v1.1.0 // indirect
@@ -27,16 +29,17 @@ require (
 	github.com/spf13/jwalterweatherman v0.0.0-20170523133247-0efa5202c046 // indirect
 	github.com/spf13/pflag v1.0.0 // indirect
 	github.com/spf13/viper v1.0.0
-	github.com/stretchr/testify v1.1.4
+	github.com/stretchr/testify v1.6.0
 	github.com/vishvananda/netlink v0.0.0-20170802012344-a95659537721
 	github.com/vishvananda/netns v0.0.0-20170707011535-86bef332bfc3 // indirect
-	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+	golang.org/x/net v0.0.0-20200528225125-3c3fba18258b
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208 // indirect
+	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20170731182057-09f6ed296fc6 // indirect
 	google.golang.org/grpc v1.5.1
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.0.0-20170721122051-25c4ec802a7d // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200601152816-913338de1bd2 // indirect
 )
 
 go 1.13
---
+++ pkg/packet/bgp/sr_policy_test.go
@@ -0,0 +1,219 @@
+package bgp
+
+import (
+	"encoding/binary"
+	"net"
+	"reflect"
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
+func TestBindingSIDRoundTrip(t *testing.T) {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, 24321)
+	bsid, _ := NewBSID(b)
+	tests := []struct {
+		name  string
+		input *TunnelEncapSubTLVSRBSID
+		fail  bool
+	}{
+		{
+			name: "no bsid",
+			input: &TunnelEncapSubTLVSRBSID{
+				TunnelEncapSubTLV: TunnelEncapSubTLV{
+					Type:   1,
+					Length: 2,
+				},
+				Flags: 0x0,
+				BSID:  nil,
+			},
+			fail: false,
+		},
+		{
+			name: "v4 bsid",
+			input: &TunnelEncapSubTLVSRBSID{
+				TunnelEncapSubTLV: TunnelEncapSubTLV{
+					Type:   1,
+					Length: 6,
+				},
+				Flags: 0x0,
+				BSID:  bsid,
+			},
+			fail: false,
+		},
+		{
+			name: "srv6 bsid",
+			input: &TunnelEncapSubTLVSRBSID{
+				TunnelEncapSubTLV: TunnelEncapSubTLV{
+					Type:   1,
+					Length: 18,
+				},
+				Flags: 0x0,
+				BSID: &BSID{
+					Value: net.ParseIP("2001:1::1").To16(),
+				},
+			},
+			fail: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := tt.input.Serialize()
+			if err != nil && !tt.fail {
+				t.Fatalf("expected to succeed but failed with error: %+v", err)
+			}
+			if err == nil && tt.fail {
+				t.Fatal("Expected to fail but succeeded")
+			}
+			if err != nil {
+				return
+			}
+			result := &TunnelEncapSubTLVSRBSID{}
+			err = result.DecodeFromBytes(b)
+			if err != nil && !tt.fail {
+				t.Fatalf("expected to succeed but failed with error: %+v", err)
+			}
+			if err == nil && tt.fail {
+				t.Fatal("Expected to fail but succeeded")
+			}
+			if err != nil {
+				return
+			}
+			if !reflect.DeepEqual(tt.input, result) {
+				t.Logf("Diffs: %+v", deep.Equal(tt.input, result))
+				t.Fatalf("expected: %+v does not match result: %+v", tt.input, result)
+			}
+		})
+	}
+}
+
+func TestSegmentListRoundTrip(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *TunnelEncapSubTLVSRSegmentList
+		fail  bool
+	}{
+		{
+			name: "empty Segment List Sub TLV",
+			input: &TunnelEncapSubTLVSRSegmentList{
+				TunnelEncapSubTLV: TunnelEncapSubTLV{
+					Type:   ENCAP_SUBTLV_TYPE_SRSEGMENT_LIST,
+					Length: 6, // Weight (6 bytes) + Length of each segment + 2
+				},
+			},
+			fail: false,
+		},
+		{
+			name: "only empty weight",
+			input: &TunnelEncapSubTLVSRSegmentList{
+				TunnelEncapSubTLV: TunnelEncapSubTLV{
+					Type:   ENCAP_SUBTLV_TYPE_SRSEGMENT_LIST,
+					Length: 6, // Weight (6 bytes) + Length of each segment + 2
+				},
+				Weight: &SegmentListWeight{
+					TunnelEncapSubTLV: TunnelEncapSubTLV{
+						Type:   9,
+						Length: 6,
+					},
+					Flags:  0,
+					Weight: 100,
+				},
+			},
+			fail: false,
+		},
+		{
+			name: "weight and 1 type A segment",
+			input: &TunnelEncapSubTLVSRSegmentList{
+				TunnelEncapSubTLV: TunnelEncapSubTLV{
+					Type:   ENCAP_SUBTLV_TYPE_SRSEGMENT_LIST,
+					Length: 6, // Weight (6 bytes) + Length of each segment + 2
+				},
+				Weight: &SegmentListWeight{
+					TunnelEncapSubTLV: TunnelEncapSubTLV{
+						Type:   SegmentListSubTLVWeight,
+						Length: 6,
+					},
+					Flags:  0,
+					Weight: 100,
+				},
+				Segments: []TunnelEncapSubTLVInterface{
+					&SegmentTypeA{
+						TunnelEncapSubTLV: TunnelEncapSubTLV{
+							Type:   EncapSubTLVType(TypeA),
+							Length: 6,
+						},
+						Flags: 0,
+						Label: (21431 << 12),
+					},
+				},
+			},
+			fail: false,
+		},
+		{
+			name: "weight and 2 type A segment",
+			input: &TunnelEncapSubTLVSRSegmentList{
+				TunnelEncapSubTLV: TunnelEncapSubTLV{
+					Type:   ENCAP_SUBTLV_TYPE_SRSEGMENT_LIST,
+					Length: 6, // Weight (6 bytes) + Length of each segment + 2
+				},
+				Weight: &SegmentListWeight{
+					TunnelEncapSubTLV: TunnelEncapSubTLV{
+						Type:   SegmentListSubTLVWeight,
+						Length: 6,
+					},
+					Flags:  0,
+					Weight: 100,
+				},
+				Segments: []TunnelEncapSubTLVInterface{
+					&SegmentTypeA{
+						TunnelEncapSubTLV: TunnelEncapSubTLV{
+							Type:   EncapSubTLVType(TypeA),
+							Length: 6,
+						},
+						Flags: 0,
+						Label: (21431 << 12),
+					},
+					&SegmentTypeA{
+						TunnelEncapSubTLV: TunnelEncapSubTLV{
+							Type:   EncapSubTLVType(TypeA),
+							Length: 6,
+						},
+						Flags: 0,
+						Label: (100001 << 12),
+					},
+				},
+			},
+			fail: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := tt.input.Serialize()
+			if err != nil && !tt.fail {
+				t.Fatalf("expected to succeed but failed with error: %+v", err)
+			}
+			if err == nil && tt.fail {
+				t.Fatal("Expected to fail but succeeded")
+			}
+			if err != nil {
+				return
+			}
+			result := &TunnelEncapSubTLVSRSegmentList{}
+			err = result.DecodeFromBytes(b)
+			if err != nil && !tt.fail {
+				t.Fatalf("expected to succeed but failed with error: %+v", err)
+			}
+			if err == nil && tt.fail {
+				t.Fatal("Expected to fail but succeeded")
+			}
+			if err != nil {
+				return
+			}
+			if !reflect.DeepEqual(tt.input, result) {
+				t.Logf("Diffs: %+v", deep.Equal(tt.input, result))
+				t.Fatalf("expected: %+v does not match result: %+v", tt.input, result)
+			}
+		})
+	}
+}
