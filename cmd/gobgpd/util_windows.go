6174855b2296ee42e88ea7e186a431fc507ecca9
Ben Renninson <ben@goldensyrupgames.com>
2022-12-25 16:58:09 +1100

    Get building on Windows
    Both `gobgpd` and `gobgpd` now build for and run on Windows. This is based on https://github.com/osrg/gobgp/pull/2250/ with less required changes. Tested and confirmed it can peer successfully and receive routes. It still builds on Linux (doesn't change syscalls for non-windows files).

--- cmd/gobgpd/util_windows.go
+++ cmd/gobgpd/util_windows.go
@@ -12,13 +12,54 @@
 // implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//go:build windows
+// +build windows
 
 package main
 
 import (
 	"errors"
+
+	"github.com/osrg/gobgp/v3/pkg/log"
+	"github.com/sirupsen/logrus"
 )
 
 func addSyslogHook(_, _ string) error {
 	return errors.New("syslog is not supported on this OS")
+}
+
+type builtinLogger struct {
+	logger *logrus.Logger
+}
+
+func (l *builtinLogger) Panic(msg string, fields log.Fields) {
+	l.logger.WithFields(logrus.Fields(fields)).Panic(msg)
+}
+
+func (l *builtinLogger) Fatal(msg string, fields log.Fields) {
+	l.logger.WithFields(logrus.Fields(fields)).Fatal(msg)
+}
+
+func (l *builtinLogger) Error(msg string, fields log.Fields) {
+	l.logger.WithFields(logrus.Fields(fields)).Error(msg)
+}
+
+func (l *builtinLogger) Warn(msg string, fields log.Fields) {
+	l.logger.WithFields(logrus.Fields(fields)).Warn(msg)
+}
+
+func (l *builtinLogger) Info(msg string, fields log.Fields) {
+	l.logger.WithFields(logrus.Fields(fields)).Info(msg)
+}
+
+func (l *builtinLogger) Debug(msg string, fields log.Fields) {
+	l.logger.WithFields(logrus.Fields(fields)).Debug(msg)
+}
+
+func (l *builtinLogger) SetLevel(level log.LogLevel) {
+	l.logger.SetLevel(logrus.Level(level))
+}
+
+func (l *builtinLogger) GetLevel() log.LogLevel {
+	return log.LogLevel(l.logger.GetLevel())
 }
