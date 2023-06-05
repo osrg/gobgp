fbeaa1c66827cb9ed339a275d7b884a9780ecc73
Rinat Baygildin <bayrinat@yandex-team.ru>
2023-06-05 13:50:30 +0300

    Do not check enabling when EOR received
    Signed-off-by: Rinat Baygildin <bayrinat@yandex-team.ru>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4639,7 +4639,7 @@ func (s *BgpServer) watch(opts ...watchOption) (w *watcher) {
 					p.fsm.lock.RLock()
 					defer p.fsm.lock.RUnlock()
 					for _, a := range p.fsm.pConf.AfiSafis {
-						if s := a.MpGracefulRestart.State; s.Enabled && s.EndOfRibReceived {
+						if s := a.MpGracefulRestart.State; s.EndOfRibReceived {
 							family := a.State.Family
 							peerInfo := &table.PeerInfo{
 								AS:           p.fsm.peerInfo.AS,
