0ab45505ac406a5381d68c5ed9920edac9f5a666
Costas Drogos <costas.drogos@gmail.com>
2023-06-06 22:55:48 +0200

    cmd/gobgp: add tests for EVPN parsing
    Add tests to validate parsing of all EVPN-related configuration.
    
    Examples taken from docs/sources/evpn.md.

--- cmd/gobgp/global_test.go
+++ cmd/gobgp/global_test.go
@@ -37,3 +37,31 @@ func Test_ParsePath(t *testing.T) {
 		i = int(a.GetType())
 	}
 }
+
+func Test_ParseEvpnPath(t *testing.T) {
+	var tests = []struct {
+		name string
+		path string
+	}{
+		{"Ethernet Auto-Discovery", "a-d esi LACP aa:bb:cc:dd:ee:ff 100 etag 200 label 300 rd 1.1.1.1:65000 rt 65000:200 encap vxlan esi-label 400 single-active"},
+		{"MAC/IP Advertisement", "macadv aa:bb:cc:dd:ee:ff 10.0.0.1 esi AS 65000 100 etag 200 label 300 rd 1.1.1.1:65000 rt 65000:400 encap vxlan default-gateway"},
+		{"I-PMSI", "i-pmsi etag 100 rd 1.1.1.1:65000 rt 65000:200 encap vxlan pmsi ingress-repl 100 1.1.1.1"},
+		{"IP Prefix", "prefix 10.0.0.0/24 172.16.0.1 esi MSTP aa:aa:aa:aa:aa:aa 100 etag 200 label 300 rd 1.1.1.1:65000 rt 65000:200 encap vxlan router-mac bb:bb:bb:bb:bb:bb"},
+		{"Multicast", "multicast 10.0.0.1 etag 100 rd 1.1.1.1:65000 rt 65000:200 encap vxlan pmsi ingress-repl 100 1.1.1.1"},
+		{"Ethernet Segment Identifier", "esi 10.0.0.1 esi MAC aa:bb:cc:dd:ee:ff 100 rd 1.1.1.1:65000 rt 65000:200 encap vxlan"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			path, err := parsePath(bgp.RF_EVPN, strings.Split(tt.path, " "))
+			assert.Nil(err)
+			i := 0
+			attrs, _ := apiutil.GetNativePathAttributes(path)
+			for _, a := range attrs {
+				assert.True(i < int(a.GetType()))
+				i = int(a.GetType())
+			}
+		})
+	}
+}
