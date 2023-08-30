b134e535c58c781a34df458a95243300b9768c3c
wenovus <wenbli@google.com>
2023-08-30 10:29:20 -0700

    Add internal config type aliases

---
+++ tools/config/alias.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ALIAS_FILE="pkg/config/bgp_aliases.go"
+
+echo "package config" > "${ALIAS_FILE}"
+echo >> "${ALIAS_FILE}"
+echo 'import "github.com/osrg/gobgp/v3/internal/pkg/config"' >> "${ALIAS_FILE}"
+echo >> "${ALIAS_FILE}"
+
+grep -E -o '^type [A-Z]\w*' internal/pkg/config/bgp_configs.go | grep -E -o '\w+$' | sed "s,\(.*\),type \1 = config.\1,g" >> "${ALIAS_FILE}"
+grep -E -o '^\s+[A-Z][0-9A-Z_]+\s+' internal/pkg/config/bgp_configs.go | grep -E -o '^\s+\w+' | grep -E -o '\w+$' | sed "s,\(.*\),const \1 = config.\1,g" >> "${ALIAS_FILE}"
