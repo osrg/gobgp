a0f653de3a7d83f9ce070077c5b3fa46fb68ba2f
watal <watal.i27e@gmail.com>
2025-03-09 10:02:46 +0900

    feat: Handle multiple Go snippets and error for missing snippets

--- test/scenario_test/ci-scripts/build_embedded_go.py
+++ test/scenario_test/ci-scripts/build_embedded_go.py
@@ -2,27 +2,62 @@ import sys
 import os
 from subprocess import call
 
+def build_embedded_go_codes(filename):
+    """
+    Extract embedded Go code snippets from a Markdown file and compile each of them.
 
-def cut(filename, out):
+    Args:
+        filename (str): The path to the Markdown file containing embedded Go code snippets.
+
+    Returns:
+        list: A list of output Go file names that were created and compiled.
+
+    Raises:
+        ValueError: If no Go code block is found in the file.
+    """
+
     with open(filename, 'r') as f:
-        flag = False
-        codes = []
+        flag = False  # Tracks if inside Go block
+        codes = []   # Go codes from embedded
+        snippet_counter = 1
+        out_files = []
+
         for line in f.readlines():
             if line.strip() == '```go':
                 flag = True
-            elif line.strip() == '```':
+                codes = []
+            elif line.strip() == '```' and flag:
+                out = f'temporary_{snippet_counter}.go'
+                out_files.append(out)
+
+                if os.path.exists(out):
+                    os.remove(out)
+
                 with open(out, 'w') as g:
                     g.write("".join(codes))
-                return
+
+                snippet_counter += 1
+                flag = False
             elif flag:
                 codes.append(line)
+
+        if not out_files:
+            raise ValueError("No Go code block found in the markdown file (argument).")
+
+        return out_files
 
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    out = 'hoge.go'
-    cut(filename, out)
-    ret = call(['go', 'build', '-o', 'hoge', out])
-    os.remove(out)
-    os.remove('hoge')
-    sys.exit(ret)
+    out_files = build_embedded_go_codes(filename)
+
+    ret_code = 0
+    for out in out_files:
+        ret = call(['go', 'build', '-o', out.replace('.go', ''), out])
+        if ret != 0:
+            ret_code = ret
+        os.remove(out)
+        if os.path.exists(out.replace('.go', '')):
+            os.remove(out.replace('.go', ''))
+
+    sys.exit(ret_code)
