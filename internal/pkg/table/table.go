1d1c567d19e0ef4da877a0bf86370fdd25e0476c
Georgy Borodin <bor1-go@yandex-team.ru>
2025-05-13 21:30:00 +0000

    Speed up RTC exchange, exclude non-VPN paths from the RTC scheme
    1. Add Key(RouteTarget)->uint64.
    2. Add map[Key(rt)]map[*Path]{} to globalRib tables for fast making path list by RT.
    3. Add map[Key(rt)]int to Adj RTC tables for fast filtering paths by RTs.
    4. Add tests on pp.1-3.

--- internal/pkg/table/table.go
+++ internal/pkg/table/table.go
@@ -183,18 +183,22 @@ type Table struct {
 	Family       bgp.Family
 	destinations Destinations
 	logger       log.Logger
+	// rtc is used for RTC rt saving. Every global vpn table has vpnPathsPart,
+	// every Adj RF_RTC_UC table has rtPathsPart, and other tables have not rtc instance.
+	rtc rtcHandler
 	// index of evpn prefixes with paths to a specific MAC in a MAC-VRF
 	// this is a map[rt, MAC address]map[addrPrefixKey][]nlri
 	// this holds a map for a set of prefixes.
 	macIndex EVPNMacNLRIs
 }
 
-func NewTable(logger log.Logger, rf bgp.Family, dsts ...*Destination) *Table {
+func newTablePartial(logger log.Logger, rf bgp.Family, isAdj bool, dsts ...*Destination) *Table {
 	t := &Table{
 		Family:       rf,
 		destinations: make(Destinations),
 		logger:       logger,
 		macIndex:     make(EVPNMacNLRIs),
+		rtc:          newRTCPart(rf, isAdj),
 	}
 	for _, dst := range dsts {
 		t.setDestination(dst)
@@ -204,6 +208,14 @@ func NewTable(logger log.Logger, rf bgp.Family, dsts ...*Destination) *Table {
 
 func (t *Table) GetFamily() bgp.Family {
 	return t.Family
+}
+
+func NewTable(logger log.Logger, rf bgp.Family, dsts ...*Destination) *Table {
+	return newTablePartial(logger, rf, false, dsts...)
+}
+
+func NewAdjTable(logger log.Logger, rf bgp.Family, dsts ...*Destination) *Table {
+	return newTablePartial(logger, rf, true, dsts...)
 }
 
 func (t *Table) deletePathsByVrf(vrf *Vrf) []*Path {
@@ -346,7 +358,16 @@ func (t *Table) getOrCreateDest(nlri bgp.AddrPrefixInterface, size int) *Destina
 func (t *Table) update(newPath *Path) *Update {
 	t.validatePath(newPath)
 	dst := t.getOrCreateDest(newPath.GetNlri(), 64)
-	u := dst.Calculate(t.logger, newPath)
+	u, oldPath := dst.Calculate(t.logger, newPath)
+
+	if t.rtc != nil {
+		if newPath.IsWithdraw {
+			t.rtc.unregister(oldPath, true)
+		} else {
+			t.rtc.unregister(oldPath, false)
+			t.rtc.register(newPath)
+		}
+	}
 
 	if len(dst.knownPathList) == 0 {
 		t.deleteDest(dst)
--- pkg/packet/bgp/bgp.go
+++ pkg/packet/bgp/bgp.go
@@ -395,6 +395,8 @@ var CapNameMap = map[BGPCapabilityCode]string{
 	BGP_CAP_SOFT_VERSION:                "software-version",
 }
 
+const DefaultRT uint64 = 0
+
 func (c BGPCapabilityCode) String() string {
 	if n, y := CapNameMap[c]; y {
 		return n
@@ -2373,6 +2375,34 @@ type RouteTargetMembershipNLRI struct {
 	Length      uint8
 	AS          uint32
 	RouteTarget ExtendedCommunityInterface
+}
+
+var (
+	ErrInvalidRouteTarget error = errors.New("ExtendedCommunity is not RouteTarget")
+	ErrNilCommunity       error = errors.New("RouteTarget could not be nil")
+)
+
+func ExtCommRouteTargetKey(routeTarget ExtendedCommunityInterface) (uint64, error) {
+	if routeTarget == nil {
+		return 0, ErrNilCommunity
+	}
+	switch rt := routeTarget.(type) {
+	case *TwoOctetAsSpecificExtended, *IPv4AddressSpecificExtended, *FourOctetAsSpecificExtended:
+		bytes, err := rt.Serialize()
+		if err != nil {
+			return 0, err
+		}
+		return binary.BigEndian.Uint64(bytes[:]), nil
+	default:
+		return 0, ErrInvalidRouteTarget
+	}
+}
+
+func (n *RouteTargetMembershipNLRI) RouteTargetKey() (uint64, error) {
+	if n.RouteTarget == nil {
+		return DefaultRT, nil
+	}
+	return ExtCommRouteTargetKey(n.RouteTarget)
 }
 
 func (n *RouteTargetMembershipNLRI) DecodeFromBytes(data []byte, options ...*MarshallingOption) error {
@@ -11971,7 +12001,7 @@ type TwoOctetAsSpecificExtended struct {
 }
 
 func (e *TwoOctetAsSpecificExtended) Serialize() ([]byte, error) {
-	buf := make([]byte, 8)
+	buf := [8]byte{}
 	if e.IsTransitive {
 		buf[0] = byte(EC_TYPE_TRANSITIVE_TWO_OCTET_AS_SPECIFIC)
 	} else {
@@ -11980,7 +12010,7 @@ func (e *TwoOctetAsSpecificExtended) Serialize() ([]byte, error) {
 	buf[1] = byte(e.SubType)
 	binary.BigEndian.PutUint16(buf[2:], e.AS)
 	binary.BigEndian.PutUint32(buf[4:], e.LocalAdmin)
-	return buf, nil
+	return buf[:], nil
 }
 
 func (e *TwoOctetAsSpecificExtended) String() string {
@@ -12025,7 +12055,7 @@ type IPv4AddressSpecificExtended struct {
 }
 
 func (e *IPv4AddressSpecificExtended) Serialize() ([]byte, error) {
-	buf := make([]byte, 8)
+	buf := [8]byte{}
 	if e.IsTransitive {
 		buf[0] = byte(EC_TYPE_TRANSITIVE_IP4_SPECIFIC)
 	} else {
@@ -12034,7 +12064,7 @@ func (e *IPv4AddressSpecificExtended) Serialize() ([]byte, error) {
 	buf[1] = byte(e.SubType)
 	copy(buf[2:6], e.IPv4)
 	binary.BigEndian.PutUint16(buf[6:], e.LocalAdmin)
-	return buf, nil
+	return buf[:], nil
 }
 
 func (e *IPv4AddressSpecificExtended) String() string {
@@ -12141,7 +12171,7 @@ type FourOctetAsSpecificExtended struct {
 }
 
 func (e *FourOctetAsSpecificExtended) Serialize() ([]byte, error) {
-	buf := make([]byte, 8)
+	buf := [8]byte{}
 	if e.IsTransitive {
 		buf[0] = byte(EC_TYPE_TRANSITIVE_FOUR_OCTET_AS_SPECIFIC)
 	} else {
@@ -12150,7 +12180,7 @@ func (e *FourOctetAsSpecificExtended) Serialize() ([]byte, error) {
 	buf[1] = byte(e.SubType)
 	binary.BigEndian.PutUint32(buf[2:], e.AS)
 	binary.BigEndian.PutUint16(buf[6:], e.LocalAdmin)
-	return buf, nil
+	return buf[:], nil
 }
 
 func (e *FourOctetAsSpecificExtended) String() string {
