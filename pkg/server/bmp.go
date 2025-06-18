250729e3ae5ffd5c6379565cbaf8d9e56efec708
Maxime Peim <mpeim@cisco.com>
2025-06-18 16:27:30 +0200

    chore: errcheck in pkg/server/bmp.go

--- pkg/server/bmp.go
+++ pkg/server/bmp.go
@@ -248,13 +248,13 @@ func (b *bmpClient) loop() {
 					}
 				case <-tickerCh:
 					var err error
-					b.s.ListPeer(context.Background(), &api.ListPeerRequest{EnableAdvertised: true},
+					listErr := b.s.ListPeer(context.Background(), &api.ListPeerRequest{EnableAdvertised: true},
 						func(peer *api.Peer) {
 							if err == nil && peer.State.SessionState == api.PeerState_SESSION_STATE_ESTABLISHED {
 								err = write(bmpPeerStats(bmp.BMP_PEER_TYPE_GLOBAL, 0, time.Now().Unix(), peer))
 							}
 						})
-					if err != nil {
+					if listErr != nil && err != nil {
 						return false
 					}
 				case <-b.dead:
