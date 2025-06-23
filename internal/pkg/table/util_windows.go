4910fe6c9609bc326bb65900d970ea1fd4dfed1e
Nicolas PLANEL <nplanel@cisco.com>
2025-06-23 10:10:07 +0200

    TestTableDestinationsCollisionAttack

---
+++ internal/pkg/table/util_windows.go
@@ -0,0 +1,14 @@
+//go:build windows
+// +build windows
+
+package table
+
+// #include <windows.h>
+import "C"
+
+func SystemMemoryAvailableMiB() uint64 {
+	MEMORYSTATUSEX status;
+    status.dwLength = sizeof(status);
+    GlobalMemoryStatusEx(&status);
+    return uint64(status.ullAvailPhys) / (1024 * 1024)
+}
