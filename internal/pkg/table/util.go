ae0a44c1a20dbcfb2efef157a60b5511f3614172
Nicolas PLANEL <nplanel@cisco.com>
2025-06-23 10:10:07 +0200

    TestTableDestinationsCollisionAttack

---
+++ internal/pkg/table/util.go
@@ -0,0 +1,11 @@
+//go:build !windows
+// +build !windows
+
+package table
+
+// #include <unistd.h>
+import "C"
+
+func SystemMemoryAvailableMiB() uint64 {
+	return uint64(C.sysconf(C._SC_AVPHYS_PAGES) * C.sysconf(C._SC_PAGE_SIZE) / (1024 * 1024))
+}
