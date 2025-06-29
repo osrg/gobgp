3dfea23300d536351bb79754bf668bf327050580
Maxime Peim <mpeim@cisco.com>
2025-06-29 17:36:06 +0200

    refactor: move config utility functions

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -817,7 +817,7 @@ func (s *BgpServer) toConfig(peer *peer, getAdvertised bool) *oc.Neighbor {
 	conf.State.RemoteCapabilityList = remoteCap
 
 	peer.fsm.lock.Lock()
-	conf.State.LocalCapabilityList = capabilitiesFromConfig(peer.fsm.pConf)
+	conf.State.LocalCapabilityList = peer.fsm.pConf.Capabilities()
 	peer.fsm.lock.Unlock()
 
 	peer.fsm.lock.RLock()
