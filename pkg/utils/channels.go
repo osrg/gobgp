3ebf75e0b2f04c11186966e3eb53308797823f42
Maxime Peim <mpeim@cisco.com>
2025-06-30 10:48:31 +0200

    feat: push with context

--- pkg/utils/channels.go
+++ pkg/utils/channels.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"context"
+
 	"github.com/eapache/channels"
 )
 
@@ -8,5 +10,25 @@ func CleanInfiniteChannel(ch *channels.InfiniteChannel) {
 	ch.Close()
 	// drain all remaining items
 	for range ch.Out() {
+	}
+}
+
+func PushWithContext[T any](ctx context.Context, ch chan<- T, item T, wait bool) bool {
+	if wait {
+		select {
+		case <-ctx.Done():
+			return false
+		case ch <- item:
+			return true
+		}
+	} else {
+		select {
+		case <-ctx.Done():
+			return false
+		case ch <- item:
+			return true
+		default:
+			return false
+		}
 	}
 }
