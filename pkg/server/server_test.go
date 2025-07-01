24559a2639d1b283d1a0f867489723742c63c5e9
Maxime Peim <mpeim@cisco.com>
2025-07-01 10:00:58 +0200

    fix: properly stop bgp
    'handlefsmMsg' function sole possible purpose was when the handler
    context is cancelled, which only happen if the 'deleteNeighbor'
    function is called. so the responsability of ending properly the
    fsm is moved back to the peer with 'stopFSMHandler'.
    this allow us to quit properly the 'Serve' goroutine.

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -1345,7 +1345,6 @@ func TestGracefulRestartTimerExpired(t *testing.T) {
 		},
 	})
 	require.NoError(t, err)
-	defer s2.StopBgp(context.Background(), &api.StopBgpRequest{})
 
 	p2 := &api.Peer{
 		Conf: &api.PeerConf{
