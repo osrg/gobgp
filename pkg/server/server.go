7e83a5b46e7f734f6b0a0339b955597c73ee06e5
Maxime Peim <mpeim@cisco.com>
2025-07-26 11:33:18 +0200

    fix: remove incoming channels field and reflection
    remove unused incoming channels from BGP server and use a plain
    select/case instead of reflection in the Serve function.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -22,7 +22,6 @@ import (
 	"fmt"
 	"net"
 	"net/netip"
-	"reflect"
 	"slices"
 	"strconv"
 	"strings"
@@ -116,7 +115,6 @@ type BgpServer struct {
 	apiServer    *server
 	bgpConfig    oc.Bgp
 	acceptCh     chan net.Conn
-	incomings    []*channels.InfiniteChannel
 	mgmtCh       chan *mgmtOp
 	policy       *table.RoutingPolicy
 	listeners    []*netutils.TCPListener
@@ -189,19 +187,6 @@ func (s *BgpServer) Stop() {
 
 	if s.apiServer != nil {
 		s.apiServer.grpcServer.Stop()
-	}
-}
-
-func (s *BgpServer) addIncoming(ch *channels.InfiniteChannel) {
-	s.incomings = append(s.incomings, ch)
-}
-
-func (s *BgpServer) delIncoming(ch *channels.InfiniteChannel) {
-	for i, c := range s.incomings {
-		if c == ch {
-			s.incomings = append(s.incomings[:i], s.incomings[i+1:]...)
-			return
-		}
 	}
 }
 
@@ -314,7 +299,7 @@ func (s *BgpServer) startFsmHandler(peer *peer) {
 			}
 
 			cleanInfiniteChannel(fsm.outgoingCh)
-			if s.shutdownWG != nil && len(s.incomings) == 0 {
+			if s.shutdownWG != nil {
 				s.shutdownWG.Done()
 			}
 			return
@@ -444,57 +429,33 @@ func (s *BgpServer) passConnToPeer(conn net.Conn) {
 	}
 }
 
-const firstPeerCaseIndex = 3
-
 func (s *BgpServer) Serve() {
 	s.listeners = make([]*netutils.TCPListener, 0, 2)
 
 	for {
-		cases := make([]reflect.SelectCase, firstPeerCaseIndex+len(s.incomings))
-		cases[0] = reflect.SelectCase{
-			Dir:  reflect.SelectRecv,
-			Chan: reflect.ValueOf(s.mgmtCh),
-		}
-		cases[1] = reflect.SelectCase{
-			Dir:  reflect.SelectRecv,
-			Chan: reflect.ValueOf(s.acceptCh),
-		}
-		cases[2] = reflect.SelectCase{
-			Dir:  reflect.SelectRecv,
-			Chan: reflect.ValueOf(s.roaManager.ReceiveROA()),
-		}
-		for i := firstPeerCaseIndex; i < len(cases); i++ {
-			cases[i] = reflect.SelectCase{
-				Dir:  reflect.SelectRecv,
-				Chan: reflect.ValueOf(s.incomings[i-firstPeerCaseIndex].Out()),
-			}
-		}
-
-		chosen, value, _ := reflect.Select(cases)
 		tStart := time.Now()
-		s.shared.mu.Lock()
-		switch chosen {
-		case 0:
-			op := value.Interface().(*mgmtOp)
+		select {
+		case op := <-s.mgmtCh:
 			tWait := tStart.Sub(op.timestamp)
+			s.shared.mu.Lock()
 			s.handleMGMTOp(op)
+			s.shared.mu.Unlock()
 			s.timingHook.Observe(FSMMgmtOp, time.Since(tStart), tWait)
-		case 1:
+		case conn := <-s.acceptCh:
 			// NOTE: it would be useful to use kernel metrics such as SO_TIMESTAMPING to record time we got
 			// first SYN packet in TCP connection. For now we skip tWait for accept events, message/mgmt op
 			// delays should be enough to analyze FSM loop.
-			conn := value.Interface().(*netutils.TCPConn)
+			s.shared.mu.Lock()
 			s.passConnToPeer(conn)
+			s.shared.mu.Unlock()
 			s.timingHook.Observe(FSMAccept, time.Since(tStart), 0)
-		case 2:
-			ev := value.Interface().(*roaEvent)
+		case ev := <-s.roaManager.ReceiveROA():
 			tWait := tStart.Sub(ev.timestamp)
+			s.shared.mu.Lock()
 			s.roaManager.HandleROAEvent(ev)
+			s.shared.mu.Unlock()
 			s.timingHook.Observe(FSMROAEvent, time.Since(tStart), tWait)
-		default:
-			panic("unexpected select case chosen")
 		}
-		s.shared.mu.Unlock()
 	}
 }
 
